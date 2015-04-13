aada4c09e99d9fd3fdfc9d2714c31fc9fe357616
Amelie <amelie.rolland.1@gmail.com>
2015-04-13 13:57:16 -0400

    add camps dataset, loader, and tests

--- preimage/datasets/loader.py
+++ preimage/datasets/loader.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 __author__ = 'amelie'
 
 import pickle
@@ -7,11 +8,17 @@ from os.path import join
 import numpy
 
 
-class Dataset:
+class StructedOutputDataset:
     def __init__(self, X, Y):
         self.X = X
         self.Y = Y
         self.y_lengths = numpy.array([len(y) for y in Y])
+
+
+class StandardDataset:
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
 
 
 def load_pickle_file(file_name):
@@ -25,6 +32,12 @@ def load_ocr_letters(fold_id=0):
     data = load_pickle_file('ocrletters.pickle')
     train_indexes = numpy.where(data['fold_ids'] == fold_id)
     test_indexes = numpy.where(data['fold_ids'] != fold_id)
-    train_dataset = Dataset(data['X'][train_indexes], data['y'][train_indexes])
-    test_dataset = Dataset(data['X'][test_indexes], data['y'][test_indexes])
+    train_dataset = StructedOutputDataset(data['X'][train_indexes], data['y'][train_indexes])
+    test_dataset = StructedOutputDataset(data['X'][test_indexes], data['y'][test_indexes])
     return train_dataset, test_dataset
+
+
+def load_camps_dataset():
+    data = load_pickle_file('camps.pickle')
+    train_dataset = StandardDataset(data['X'], data['y'])
+    return train_dataset
--- preimage/tests/datasets/loader.py
+++ preimage/tests/datasets/loader.py
@@ -2,14 +2,17 @@ __author__ = 'amelie'
 
 import unittest2
 
-from preimage.datasets.loader import load_ocr_letters
+from preimage.datasets.loader import load_ocr_letters, load_camps_dataset
 
 
-# This is just to check the pickle files were made correctly (not real unit testing since we use the real .pickle files)
+# This is just to ensure the pickle files were made correctly
+# (not real unit testing since we use the real .pickle files)
 class TestLoader(unittest2.TestCase):
     def setUp(self):
         self.ocr_n_examples_in_fold = [626, 704, 684, 698, 693, 651, 739, 717, 690, 675]
         self.ocr_n_examples_not_in_fold = [6251, 6173, 6193, 6179, 6184, 6226, 6138, 6160, 6187, 6202]
+        self.camps_n_examples = 101
+        self.bpps_n_examples = 31
 
     def test_load_ocr_letters_has_correct_number_of_x_in_fold(self):
         for fold_id in range(10):
@@ -38,6 +41,16 @@ class TestLoader(unittest2.TestCase):
                 train_dataset, test_dataset = load_ocr_letters(fold_id)
                 n_examples = test_dataset.Y.shape[0]
                 self.assertEqual(n_examples, self.ocr_n_examples_not_in_fold[fold_id])
+
+    def test_load_camps_dataset_has_correct_number_of_x(self):
+        dataset = load_camps_dataset()
+        n_examples = dataset.X.shape[0]
+        self.assertEqual(n_examples, self.camps_n_examples)
+
+    def test_load_camps_dataset_has_correct_number_of_y(self):
+        dataset = load_camps_dataset()
+        n_examples = dataset.y.shape[0]
+        self.assertEqual(n_examples, self.camps_n_examples)
 
 
 if __name__ == '__main__':
