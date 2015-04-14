d4e4fa186d0d2caa6e7a3707f57e9e22e26d940a
Amelie <amelie.rolland.1@gmail.com>
2015-04-14 13:45:57 -0400

    add levenshtein loss + remove repetitive tests of other metrics

--- preimage/metrics/structured_output.py
+++ preimage/metrics/structured_output.py
@@ -1,34 +1,33 @@
 __author__ = 'amelie'
 
 import numpy
+from jellyfish import levenshtein_distance
 
 
 def zero_one_loss(Y_true, Y_predicted):
     Y_true = numpy.array(Y_true)
     Y_predicted = numpy.array(Y_predicted)
     __check_same_number_of_y(Y_true, Y_predicted)
-    n_examples = Y_true.shape[0]
-    errors = [Y_true[i] != Y_predicted[i] for i in range(n_examples)]
-    loss = numpy.mean(errors)
+    n_errors = __get_n_errors_for_each_y_predicted(Y_true, Y_predicted)
+    loss = numpy.mean(n_errors)
     return loss
+
+
+def __get_n_errors_for_each_y_predicted(Y_true, Y_predicted):
+    n_errors = [y_true != y_predicted for (y_true, y_predicted) in zip(Y_true, Y_predicted)]
+    return numpy.array(n_errors)
 
 
 def hamming_loss(Y_true, Y_predicted):
     Y_true = numpy.array(Y_true)
     Y_predicted = numpy.array(Y_predicted)
+    __check_same_number_of_y(Y_true, Y_predicted)
     y_true_lengths = __get_length_of_each_y(Y_true)
     y_predicted_lengths = __get_length_of_each_y(Y_predicted)
-    __check_same_number_of_y(Y_true, Y_predicted)
     __check_each_tuple_y_true_y_predicted_has_same_length(y_true_lengths, y_predicted_lengths)
-    n_errors = __get_n_letter_errors_for_each_y_prediction(Y_true, Y_predicted, y_true_lengths)
+    n_errors = __get_n_letter_errors_for_each_y_predicted(Y_true, Y_predicted, y_true_lengths)
     loss = numpy.mean(n_errors / y_true_lengths)
     return loss
-
-
-def __check_same_number_of_y(Y_true, Y_predicted):
-    if Y_true.shape[0] != Y_predicted.shape[0]:
-        raise ValueError('Number of Y_true must equal number of Y_predicted.'
-                         'Got {:d} Y_true, {:d} Y_predicted'.format(Y_true.shape[0], Y_predicted.shape[0]))
 
 
 def __get_length_of_each_y(Y):
@@ -41,7 +40,33 @@ def __check_each_tuple_y_true_y_predicted_has_same_length(y_true_lengths, y_pred
         raise ValueError('Each tuple (y_true, y_predicted) must have the same length ')
 
 
-def __get_n_letter_errors_for_each_y_prediction(Y_true, Y_predicted, y_lengths):
+def __get_n_letter_errors_for_each_y_predicted(Y_true, Y_predicted, y_lengths):
     n_errors = [sum([y_predicted[i] != y_true[i] for i in range(y_lengths[index])])
                 for index, (y_predicted, y_true) in enumerate(zip(Y_predicted, Y_true))]
     return numpy.array(n_errors)
+
+
+def levenshtein_loss(Y_true, Y_predicted):
+    Y_true = numpy.array(Y_true)
+    Y_predicted = numpy.array(Y_predicted)
+    __check_same_number_of_y(Y_true, Y_predicted)
+    max_lengths = __get_max_length_of_each_tuple_y_true_y_predicted(Y_true, Y_predicted)
+    distances = __get_levenshtein_distance_for_each_y_predicted(Y_true, Y_predicted)
+    loss = numpy.mean(distances / max_lengths)
+    return loss
+
+
+def __check_same_number_of_y(Y_true, Y_predicted):
+    if Y_true.shape[0] != Y_predicted.shape[0]:
+        raise ValueError('Number of Y_true must equal number of Y_predicted.'
+                         'Got {:d} Y_true, {:d} Y_predicted'.format(Y_true.shape[0], Y_predicted.shape[0]))
+
+
+def __get_max_length_of_each_tuple_y_true_y_predicted(Y_true, Y_predicted):
+    max_lengths = [max(len(y_true), len(y_predicted)) for (y_true, y_predicted) in zip(Y_true, Y_predicted)]
+    return numpy.array(max_lengths)
+
+
+def __get_levenshtein_distance_for_each_y_predicted(Y_true, Y_predicted):
+    distances = [levenshtein_distance(y_true, y_predicted) for (y_true, y_predicted) in zip(Y_true, Y_predicted)]
+    return numpy.array(distances)
--- preimage/tests/metrics/structured_output.py
+++ preimage/tests/metrics/structured_output.py
@@ -2,7 +2,7 @@ __author__ = 'amelie'
 
 import unittest2
 
-from preimage.metrics.structured_output import zero_one_loss, hamming_loss
+from preimage.metrics.structured_output import zero_one_loss, hamming_loss, levenshtein_loss
 
 
 class TestStructuredOutputMetrics(unittest2.TestCase):
@@ -13,25 +13,19 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
         self.unicor = ['unicor']
         self.potato = ['potato']
         self.tomato = ['tomato']
+        self.a = ['a']
         self.unicorn_potato = self.unicorn + self.potato
         self.unicor_tomato = self.unicor + self.tomato
         self.unicor_potato = self.unicor + self.potato
         self.unicarn_tomato = self.unicarn + self.tomato
+        self.nicornu_tomato = self.nicornu + self.tomato
 
     def test_same_word_zero_one_loss_is_zero(self):
         loss = zero_one_loss(self.unicorn, self.unicorn)
         self.assertEqual(loss, 0.)
 
-    def test_same_words_zero_one_loss_is_zero(self):
-        loss = zero_one_loss(self.unicorn_potato, self.unicorn_potato)
-        self.assertEqual(loss, 0.)
-
     def test_not_same_word_zero_one_loss_is_one(self):
         loss = zero_one_loss(self.unicorn, self.unicor)
-        self.assertEqual(loss, 1.)
-
-    def test_not_same_words_zero_one_loss_is_one(self):
-        loss = zero_one_loss(self.unicorn_potato, self.unicor_tomato)
         self.assertEqual(loss, 1.)
 
     def test_one_same_word_on_two_zero_one_loss_is_one_half(self):
@@ -44,10 +38,6 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
 
     def test_same_word_hamming_loss_is_zero(self):
         loss = hamming_loss(self.unicorn, self.unicorn)
-        self.assertEqual(loss, 0.)
-
-    def test_same_words_hamming_loss_is_zero(self):
-        loss = hamming_loss(self.unicorn_potato, self.unicorn_potato)
         self.assertEqual(loss, 0.)
 
     def test_one_wrong_letter_on_seven_hamming_loss_is_one_on_seven(self):
@@ -70,6 +60,31 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
     def test_not_same_letter_count_hamming_loss_throws_value_error(self):
         with self.assertRaises(ValueError):
             hamming_loss(self.unicorn, self.unicor)
+
+    def test_same_word_levenshtein_loss_is_zero(self):
+        loss = levenshtein_loss(self.unicorn, self.unicorn)
+        self.assertEqual(loss, 0.)
+
+    def test_one_missing_letter_on_seven_levenshtein_loss_is_one_on_seven(self):
+        loss = levenshtein_loss(self.unicorn, self.unicor)
+        self.assertEqual(loss, 1./7)
+
+    def test_one_extra_letter_on_seven_levenshtein_loss_is_one_on_seven(self):
+        loss = levenshtein_loss(self.unicor, self.unicorn)
+        self.assertEqual(loss, 1./7)
+
+    def test_no_letters_in_common_levenshtein_loss_is_one(self):
+        loss = levenshtein_loss(self.unicorn, self.a)
+        self.assertEqual(loss, 1.)
+
+    def test_two_words_with_errors_levenshtein_loss_is_mean_of_errors(self):
+        loss = levenshtein_loss(self.unicorn_potato, self.nicornu_tomato)
+        mean_errors = (2./7 + 2./6) / 2
+        self.assertEqual(loss, mean_errors)
+
+    def test_not_same_word_count_levenshtein_loss_throws_value_error(self):
+        with self.assertRaises(ValueError):
+            levenshtein_loss(self.unicorn_potato, self.unicorn)
 
 
 if __name__ == '__main__':
