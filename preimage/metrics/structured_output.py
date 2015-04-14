07feaffbb5750bfa4f4071e9e8a549b0c715d2d4
Amelie <amelie.rolland.1@gmail.com>
2015-04-14 12:03:34 -0400

    add hamming loss metric

--- preimage/metrics/structured_output.py
+++ preimage/metrics/structured_output.py
@@ -13,7 +13,35 @@ def zero_one_loss(Y_true, Y_predicted):
     return loss
 
 
+def hamming_loss(Y_true, Y_predicted):
+    Y_true = numpy.array(Y_true)
+    Y_predicted = numpy.array(Y_predicted)
+    y_true_lengths = __get_length_of_each_y(Y_true)
+    y_predicted_lengths = __get_length_of_each_y(Y_predicted)
+    __check_same_number_of_y(Y_true, Y_predicted)
+    __check_each_tuple_y_true_y_predicted_has_same_length(y_true_lengths, y_predicted_lengths)
+    n_errors = __get_n_letter_errors_for_each_y_prediction(Y_true, Y_predicted, y_true_lengths)
+    loss = numpy.mean(n_errors / y_true_lengths)
+    return loss
+
+
 def __check_same_number_of_y(Y_true, Y_predicted):
     if Y_true.shape[0] != Y_predicted.shape[0]:
         raise ValueError('Number of Y_true must equal number of Y_predicted.'
                          'Got {:d} Y_true, {:d} Y_predicted'.format(Y_true.shape[0], Y_predicted.shape[0]))
+
+
+def __get_length_of_each_y(Y):
+    y_lengths = numpy.array([len(y) for y in Y])
+    return y_lengths
+
+
+def __check_each_tuple_y_true_y_predicted_has_same_length(y_true_lengths, y_predicted_lengths):
+    if not numpy.array_equal(y_true_lengths, y_predicted_lengths):
+        raise ValueError('Each tuple (y_true, y_predicted) must have the same length ')
+
+
+def __get_n_letter_errors_for_each_y_prediction(Y_true, Y_predicted, y_lengths):
+    n_errors = [sum([y_predicted[i] != y_true[i] for i in range(y_lengths[index])])
+                for index, (y_predicted, y_true) in enumerate(zip(Y_predicted, Y_true))]
+    return numpy.array(n_errors)
--- preimage/tests/metrics/structured_output.py
+++ preimage/tests/metrics/structured_output.py
@@ -2,18 +2,21 @@ __author__ = 'amelie'
 
 import unittest2
 
-from preimage.metrics.structured_output import zero_one_loss
+from preimage.metrics.structured_output import zero_one_loss, hamming_loss
 
 
 class TestStructuredOutputMetrics(unittest2.TestCase):
     def setUp(self):
         self.unicorn = ['unicorn']
+        self.unicarn = ['unicarn']
+        self.nicornu = ['nicornu']
         self.unicor = ['unicor']
         self.potato = ['potato']
         self.tomato = ['tomato']
         self.unicorn_potato = self.unicorn + self.potato
         self.unicor_tomato = self.unicor + self.tomato
         self.unicor_potato = self.unicor + self.potato
+        self.unicarn_tomato = self.unicarn + self.tomato
 
     def test_same_word_zero_one_loss_is_zero(self):
         loss = zero_one_loss(self.unicorn, self.unicorn)
@@ -38,6 +41,35 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
     def test_not_same_word_count_zero_one_loss_throws_value_error(self):
         with self.assertRaises(ValueError):
             zero_one_loss(self.unicorn_potato, self.unicorn)
+
+    def test_same_word_hamming_loss_is_zero(self):
+        loss = hamming_loss(self.unicorn, self.unicorn)
+        self.assertEqual(loss, 0.)
+
+    def test_same_words_hamming_loss_is_zero(self):
+        loss = hamming_loss(self.unicorn_potato, self.unicorn_potato)
+        self.assertEqual(loss, 0.)
+
+    def test_one_wrong_letter_on_seven_hamming_loss_is_one_on_seven(self):
+        loss = hamming_loss(self.unicorn, self.unicarn)
+        self.assertEqual(loss, 1./7)
+
+    def test_all_wrong_letters_hamming_loss_is_one(self):
+        loss = hamming_loss(self.unicorn, self.nicornu)
+        self.assertEqual(loss, 1.)
+
+    def test_two_words_with_errors_hamming_loss_is_mean_of_errors(self):
+        loss = hamming_loss(self.unicorn_potato, self.unicarn_tomato)
+        mean_errors = (1./7 + 2./6) / 2
+        self.assertEqual(loss, mean_errors)
+
+    def test_not_same_word_count_hamming_loss_throws_value_error(self):
+        with self.assertRaises(ValueError):
+            hamming_loss(self.unicorn_potato, self.unicorn)
+
+    def test_not_same_letter_count_hamming_loss_throws_value_error(self):
+        with self.assertRaises(ValueError):
+            hamming_loss(self.unicorn, self.unicor)
 
 
 if __name__ == '__main__':
