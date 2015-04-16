19b2e8886b51777ef359f750db51dd4973732f1d
Amelie <amelie.rolland.1@gmail.com>
2015-04-16 12:50:14 -0400

    add n_gram_feature_space construction

---
+++ preimage/inference/n_gram_feature_space.py
@@ -0,0 +1,51 @@
+__author__ = 'amelie'
+
+from collections import Counter
+
+import numpy
+from scipy.sparse import csr_matrix
+from preimage.utils.alphabet import get_n_gram_to_index
+
+# Sparse matrix representation of the n_grams in each word (y)
+class NGramFeatureSpace:
+    def __init__(self, n, alphabet, Y):
+        n_gram_to_index = get_n_gram_to_index(alphabet, n)
+        self._N_gram_feature_space = self._build_feature_space(n, n_gram_to_index, numpy.array(Y))
+
+    def _build_feature_space(self, n, n_gram_to_index, Y):
+        n_examples = Y.shape[0]
+        indice_pointers, columns, data = self._initialize_indice_pointers_columns_data(n_examples)
+        for y_index, y in enumerate(Y):
+            self._build_y_indice_pointer_columns_data(indice_pointers, columns, data, n, n_gram_to_index, y, y_index)
+        N_gram_feature_space = csr_matrix((numpy.array(data), numpy.array(columns), numpy.array(indice_pointers)),
+                                          shape=(n_examples, len(n_gram_to_index)))
+        return N_gram_feature_space
+
+    def _initialize_indice_pointers_columns_data(self, n_examples):
+        indice_pointers = numpy.empty(n_examples + 1, dtype=numpy.int)
+        indice_pointers[0] = 0
+        columns = []
+        data = []
+        return indice_pointers, columns, data
+
+    def _build_y_indice_pointer_columns_data(self, indice_pointers, columns, data, n, n_gram_to_index, y, y_index):
+        y_column_indexes = self._get_y_column_indexes(y, n, n_gram_to_index)
+        y_unique_column_indexes = list(numpy.unique(y_column_indexes))
+        indice_pointers[y_index + 1] = self._get_y_indice_pointer(y_index, indice_pointers, y_unique_column_indexes)
+        columns += y_unique_column_indexes
+        data += self._get_y_data(y_column_indexes, y_unique_column_indexes)
+
+    def _get_y_indice_pointer(self, y_index, indice_pointers, y_unique_column_indexes):
+        indice_pointer = indice_pointers[y_index] + len(y_unique_column_indexes)
+        return indice_pointer
+
+    def _get_y_column_indexes(self, y, n, n_gram_to_index):
+        y_n_gram_count = len(y) - n + 1
+        column_indexes = [n_gram_to_index[y[i:i + n]] for i in range(y_n_gram_count)]
+        return column_indexes
+
+    # There is probably a faster way to do this but it must keep the index_count in index increasing order
+    def _get_y_data(self, y_n_gram_indexes, unique_n_gram_indexes):
+        n_gram_counter = Counter(y_n_gram_indexes)
+        data = [n_gram_counter[index] for index in unique_n_gram_indexes]
+        return data
---
+++ preimage/tests/inference/test_n_gram_feature_space.py
@@ -0,0 +1,61 @@
+__author__ = 'amelie'
+
+import unittest2
+import numpy.testing
+from mock import patch, MagicMock
+
+from preimage.inference.n_gram_feature_space import NGramFeatureSpace
+
+
+# Todo add test on bad constructor value : n <=0
+# Todo add test + add code for errors when one_gram_to_index doesn't contain n_gram
+class TestNGramFeatureSpace(unittest2.TestCase):
+    def setUp(self):
+        self.alphabet = ['a', 'b']
+        self.one_gram_to_index = {'a': 0, 'b': 1}
+        self.two_gram_to_index = {'aa': 0, 'ab': 1, 'ba': 2, 'bb': 3}
+        self.b = ['b']
+        self.abb = ['abb']
+        self.abaaa = ['abaaa']
+        self.abb_abaaa = self.abb + self.abaaa
+        self.b_one_gram_feature_space = [[0, 1]]
+        self.abb_one_gram_feature_space = [[1, 2]]
+        self.abaaa_two_gram_feature_space = [[2, 1, 1, 0]]
+        self.abb_abaaa_two_gram_feature_space = [[0, 1, 0, 1], [2, 1, 1, 0]]
+        self.n_gram_to_index_patch = patch('preimage.inference.n_gram_feature_space.get_n_gram_to_index')
+
+    def test_one_gram_one_letter_example_n_gram_feature_space_has_one_n_gram(self):
+        self.n_gram_to_index_patch.start().return_value = self.one_gram_to_index
+
+        feature_space = NGramFeatureSpace(n=1, alphabet=self.alphabet, Y=self.b)
+        dense_feature_space = feature_space._N_gram_feature_space.toarray()
+
+        numpy.testing.assert_array_equal(dense_feature_space, self.b_one_gram_feature_space)
+
+    def test_one_gram_three_letter_example_n_gram_feature_space_has_three_n_grams(self):
+        self.n_gram_to_index_patch.start().return_value = self.one_gram_to_index
+
+        feature_space = NGramFeatureSpace(n=1, alphabet=self.alphabet, Y=self.abb)
+        dense_feature_space = feature_space._N_gram_feature_space.toarray()
+
+        numpy.testing.assert_array_equal(dense_feature_space, self.abb_one_gram_feature_space)
+
+    def test_two_gram_five_letter_example_n_gram_feature_space_has_four_two_grams(self):
+        self.n_gram_to_index_patch.start().return_value = self.two_gram_to_index
+
+        feature_space = NGramFeatureSpace(n=2, alphabet=self.alphabet, Y=self.abaaa)
+        dense_feature_space = feature_space._N_gram_feature_space.toarray()
+
+        numpy.testing.assert_array_equal(dense_feature_space, self.abaaa_two_gram_feature_space)
+
+    def test_two_gram_two_examples_n_gram_feature_space_builds_expected_feature_space(self):
+        self.n_gram_to_index_patch.start().return_value = self.two_gram_to_index
+        
+        feature_space = NGramFeatureSpace(n=2, alphabet=self.alphabet, Y=self.abb_abaaa)
+        dense_feature_space = feature_space._N_gram_feature_space.toarray()
+
+        numpy.testing.assert_array_equal(dense_feature_space, self.abb_abaaa_two_gram_feature_space)
+
+
+if __name__ == '__main__':
+    unittest2.main()
