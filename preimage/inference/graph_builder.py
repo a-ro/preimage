8ea5337e994d72bf1f869a79e623865a6f76e5e3
Amelie <amelie.rolland.1@gmail.com>
2015-04-22 12:04:22 -0400

    add find max string in graph builder

--- preimage/inference/graph_builder.py
+++ preimage/inference/graph_builder.py
@@ -2,27 +2,31 @@ __author__ = 'amelie'
 
 import numpy
 
+from preimage.utils.alphabet import get_index_to_n_gram
+
 
 class GraphBuilder:
     def __init__(self, alphabet, n):
-        self.alphabet = alphabet
-        self.n = int(n)
-        self._n_gram_count = len(self.alphabet) ** self.n
-        self._axis = 0 if self.n == 1 else 1
+        self._alphabet = alphabet
+        self._n = int(n)
+        self._n_gram_count = len(self._alphabet) ** self._n
         self._entering_edges = self._get_entering_edge_indexes(self._n_gram_count, alphabet, n)
+        self._index_to_n_gram = get_index_to_n_gram(alphabet, self._n)
+        self._n_gram_indexes = 0 if n == 1 else numpy.arange(0, self._n_gram_count)
 
     def _get_entering_edge_indexes(self, n_gram_count, alphabet, n):
         if n == 1:
-            entering_edges = numpy.arange(0, len(alphabet))
+            entering_edges = numpy.array([numpy.arange(0, len(alphabet))])
         else:
-            step_size = len(self.alphabet) ** (n - 1)
+            step_size = len(self._alphabet) ** (n - 1)
             entering_edges = [numpy.tile(numpy.arange(i, n_gram_count, step_size), len(alphabet))
                               for i in range(step_size)]
             entering_edges = numpy.array(entering_edges).reshape(n_gram_count, len(alphabet))
         return entering_edges
 
+    # todo add verification for graph weights shape
     def build_graph(self, Graph_weights, y_length):
-        n_partitions = y_length - self.n + 1
+        n_partitions = y_length - self._n + 1
         Graph = numpy.empty((n_partitions, self._n_gram_count))
         if Graph_weights.ndim == 1:
             self._build_graph_same_weights(n_partitions, Graph, Graph_weights)
@@ -31,11 +35,44 @@ class GraphBuilder:
         return Graph
 
     def _build_graph_same_weights(self, n_partitions, Graph, graph_weights):
-        Graph[0, :] = numpy.copy(graph_weights)
+        Graph[0, :] = graph_weights
         for i in range(1, n_partitions):
-            Graph[i, :] = numpy.max(Graph[i - 1, self._entering_edges], axis=self._axis) + graph_weights
+            Graph[i, :] = numpy.max(Graph[i - 1, self._entering_edges], axis=1) + graph_weights
 
     def _build_graph_different_weights(self, n_partitions, Graph, Graph_weights):
-        Graph[0, :] = numpy.copy(Graph_weights[0, :])
+        Graph[0, :] = Graph_weights[0, :]
         for i in range(1, n_partitions):
-            Graph[i, :] = numpy.max(Graph[i - 1, self._entering_edges], axis=self._axis) + Graph_weights[i, :]
+            Graph[i, :] = numpy.max(Graph[i - 1, self._entering_edges], axis=1) + Graph_weights[i, :]
+
+    # todo add verification for graph weights shape
+    def find_max_string_in_graph(self, Graph_weights, y_length):
+        n_partitions = y_length - self._n + 1
+        Graph = numpy.empty((2, self._n_gram_count))
+        Predecessors = numpy.empty((n_partitions - 1, self._n_gram_count), dtype=numpy.int)
+        Graph[0, :] = self._get_weights(0, Graph_weights)
+        self._build_graph_with_predecessors(n_partitions, Graph, Graph_weights, Predecessors)
+        max_string_last_index = numpy.argmax(Graph[0, :])
+        max_string = self._build_max_string(n_partitions, Predecessors, max_string_last_index)
+        return max_string
+
+    def _build_graph_with_predecessors(self, n_partitions, Graph, Graph_weights, Predecessors):
+        for i in range(1, n_partitions):
+            max_entering_edge_indexes = numpy.argmax(Graph[0, self._entering_edges], axis=1)
+            Predecessors[i - 1, :] = self._entering_edges[self._n_gram_indexes, max_entering_edge_indexes]
+            Graph[1, :] = Graph[0, Predecessors[i - 1, :]] + self._get_weights(i, Graph_weights)
+            Graph[0, :] = Graph[1, :]
+
+    def _get_weights(self, i, Graph_weights):
+        if Graph_weights.ndim == 1:
+            graph_weights = Graph_weights
+        else:
+            graph_weights = Graph_weights[i, :]
+        return graph_weights
+
+    def _build_max_string(self, n_partitions, Predecessors, max_string_last_index):
+        max_string = self._index_to_n_gram[max_string_last_index]
+        best_index = max_string_last_index
+        for i in range(n_partitions - 2, -1, -1):
+            best_index = Predecessors[i, best_index]
+            max_string = self._index_to_n_gram[best_index][0] + max_string
+        return max_string
--- preimage/tests/inference/test_graph_builder.py
+++ preimage/tests/inference/test_graph_builder.py
@@ -26,6 +26,12 @@ class TestGraphBuilder(unittest2.TestCase):
                                                                      [8, 9, 10, 10, 11, 12, 12, 13, 14]])
         self.Graph_three_gram_two_partitions_2d_weights = numpy.array([numpy.arange(8), [4, 4, 5, 5, 6, 6, 7, 7]])
 
+        self.y_max_one_gram_length_one = 'c'
+        self.y_max_two_gram_length_two = 'cc'
+        self.y_max_one_gram_length_two = 'ca'
+        self.y_max_one_gram_length_three_same_weight = 'ccc'
+        self.y_max_two_gram_length_three = 'acc'
+
     def test_one_gram_length_one_build_graph_returns_graph_with_one_partition(self):
         graph_builder = GraphBuilder(self.alphabet, n=1)
 
@@ -95,6 +101,41 @@ class TestGraphBuilder(unittest2.TestCase):
         Graph = graph_builder.build_graph(self.Weights_three_gram_length_four, y_length=4)
 
         numpy.testing.assert_array_equal(Graph, self.Graph_three_gram_two_partitions_2d_weights)
+
+    def test_one_gram_length_one_find_max_string_returns_n_gram_with_max_weight(self):
+        graph_builder = GraphBuilder(self.alphabet, n=1)
+
+        y_max = graph_builder.find_max_string_in_graph(self.Weights_one_gram_length_one, y_length=1)
+
+        numpy.testing.assert_array_equal(y_max, self.y_max_one_gram_length_one)
+
+    def test_two_gram_length_two_find_max_string_returns_n_gram_with_max_weight(self):
+        graph_builder = GraphBuilder(self.alphabet, n=2)
+
+        y_max = graph_builder.find_max_string_in_graph(self.Weights_two_gram_length_two, y_length=2)
+
+        numpy.testing.assert_array_equal(y_max, self.y_max_two_gram_length_two)
+
+    def test_one_gram_length_two_find_max_string_returns_expected_string(self):
+        graph_builder = GraphBuilder(self.alphabet, n=1)
+
+        y_max = graph_builder.find_max_string_in_graph(self.Weights_one_gram_length_two, y_length=2)
+
+        numpy.testing.assert_array_equal(y_max, self.y_max_one_gram_length_two)
+
+    def test_one_gram_length_three_same_weights_find_max_string_returns_expected_string(self):
+        graph_builder = GraphBuilder(self.alphabet, n=1)
+
+        y_max = graph_builder.find_max_string_in_graph(self.weights_one_gram, y_length=3)
+
+        numpy.testing.assert_array_equal(y_max, self.y_max_one_gram_length_three_same_weight)
+
+    def test_two_gram_length_three_find_max_string_returns_expected_string(self):
+        graph_builder = GraphBuilder(self.alphabet, n=2)
+
+        y_max = graph_builder.find_max_string_in_graph(self.Weights_two_gram_length_three, y_length=3)
+
+        numpy.testing.assert_array_equal(y_max, self.y_max_two_gram_length_three)
 
 
 if __name__ == '__main__':
