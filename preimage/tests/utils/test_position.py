9d3920f74d1f60eff9a08fb9fccda407441d69d9
Amelie <amelie.rolland.1@gmail.com>
2015-05-01 15:28:35 -0400

    add position weights matrix

--- preimage/tests/utils/test_position.py
+++ preimage/tests/utils/test_position.py
@@ -3,7 +3,7 @@ __author__ = 'amelie'
 import unittest2
 import numpy.testing
 
-from preimage.utils.position import compute_position_weights
+from preimage.utils.position import compute_position_weights, compute_position_weights_matrix
 
 
 class TestPosition(unittest2.TestCase):
@@ -11,6 +11,9 @@ class TestPosition(unittest2.TestCase):
         self.position_weights_one_sigma_p_index_zero = [1., 0.6065307, 0.1353353]
         self.position_weights_one_sigma_p_index_one = [0.6065307, 1., 0.6065307]
         self.position_weights_half_sigma_p_index_zero = [1., 0.1353353, 0.0003355]
+        self.Position_weights_one_sigma_p_three_positions = [[1., 0.6065307, 0.1353353], [0.6065307, 1., 0.6065307],
+                                                             [0.1353353, 0.6065307, 1.]]
+        self.Position_weights_half_sigma_p_two_positions = [[1., 0.1353353], [0.1353353, 1.]]
 
     def test_one_sigma_p_index_zero_to_one_compute_position_weights_returns_expected_weights(self):
         position_weights = compute_position_weights(position_index=0, max_position=1, sigma_position=1)
@@ -31,6 +34,21 @@ class TestPosition(unittest2.TestCase):
         position_weights = compute_position_weights(position_index=0, max_position=3, sigma_position=0.5)
 
         numpy.testing.assert_array_almost_equal(position_weights, self.position_weights_half_sigma_p_index_zero)
+
+    def test_one_sigma_p_one_position_compute_position_weights_matrix_returns_one_weight(self):
+        position_weights = compute_position_weights_matrix(max_position=1, sigma_position=1)
+
+        numpy.testing.assert_array_almost_equal(position_weights, [[1.]])
+
+    def test_one_sigma_p_three_positions_compute_position_weights_matrix_returns_expected_weights(self):
+        position_weights = compute_position_weights_matrix(max_position=3, sigma_position=1)
+
+        numpy.testing.assert_array_almost_equal(position_weights, self.Position_weights_one_sigma_p_three_positions)
+
+    def test_half_sigma_p_two_positions_compute_position_weights_matrix_returns_expected_weights(self):
+        position_weights = compute_position_weights_matrix(max_position=2, sigma_position=0.5)
+
+        numpy.testing.assert_array_almost_equal(position_weights, self.Position_weights_half_sigma_p_two_positions)
 
 
 if __name__ == '__main__':
--- preimage/utils/position.py
+++ preimage/utils/position.py
@@ -7,3 +7,10 @@ def compute_position_weights(position_index, max_position, sigma_position):
     position_penalties = numpy.array([(position_index - j) ** 2 for j in range(max_position)], dtype=numpy.float)
     position_penalties /= -2. * (sigma_position ** 2)
     return numpy.exp(position_penalties)
+
+
+def compute_position_weights_matrix(max_position, sigma_position):
+    Position_penalties = numpy.array([(i - j) for i in range(max_position) for j in range(max_position)])
+    Position_penalties = Position_penalties.reshape(max_position, max_position)
+    Position_penalties = numpy.square(Position_penalties) / (-2 * (sigma_position ** 2))
+    return numpy.exp(Position_penalties)
