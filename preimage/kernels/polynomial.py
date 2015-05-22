b0fd2cc26633f851ae9c9045ed2295f7aedbdf93
Amelie <amelie.rolland.1@gmail.com>
2015-05-22 13:01:12 -0400

    rename uppercase parameters to lowercase except for X and Y

--- preimage/kernels/polynomial.py
+++ preimage/kernels/polynomial.py
@@ -3,6 +3,7 @@ __author__ = 'amelie'
 import numpy
 from sklearn.base import BaseEstimator
 
+
 class PolynomialKernel(BaseEstimator):
     def __init__(self, degree=2, bias=1., is_normalized=True):
         self.degree = degree
@@ -12,18 +13,18 @@ class PolynomialKernel(BaseEstimator):
     def __call__(self, X_one, X_two):
         X_one = numpy.array(X_one)
         X_two = numpy.array(X_two)
-        Gram_matrix = (numpy.dot(X_one, X_two.T) + self.bias) ** self.degree
+        gram_matrix = (numpy.dot(X_one, X_two.T) + self.bias) ** self.degree
         if self.is_normalized:
-            Gram_matrix = self._normalize_gram_matrix(X_one, X_two, Gram_matrix)
-        return Gram_matrix
+            gram_matrix = self._normalize_gram_matrix(X_one, X_two, gram_matrix)
+        return gram_matrix
 
-    def _normalize_gram_matrix(self, X_one, X_two, Gram_matrix):
-        X_one_diagonal = self._compute_x_x_similarity(X_one)
-        X_two_diagonal = self._compute_x_x_similarity(X_two)
-        Gram_matrix = ((Gram_matrix / numpy.sqrt(X_one_diagonal)).T / numpy.sqrt(X_two_diagonal)).T
-        return Gram_matrix
+    def _normalize_gram_matrix(self, X_one, X_two, gram_matrix):
+        x_one_diagonal = self._compute_element_wise_similarity(X_one)
+        x_two_diagonal = self._compute_element_wise_similarity(X_two)
+        gram_matrix = ((gram_matrix / numpy.sqrt(x_one_diagonal)).T / numpy.sqrt(x_two_diagonal)).T
+        return gram_matrix
 
-    def _compute_x_x_similarity(self, X):
-        x_x_similarity = ((X*X).sum(axis=1) + self.bias) ** self.degree
+    def _compute_element_wise_similarity(self, X):
+        x_x_similarity = ((X * X).sum(axis=1) + self.bias) ** self.degree
         x_x_similarity = x_x_similarity.reshape(-1, 1)
         return x_x_similarity
--- preimage/tests/kernels/test_polynomial.py
+++ preimage/tests/kernels/test_polynomial.py
@@ -10,46 +10,46 @@ class TestPolynomialKernel(unittest2.TestCase):
     def setUp(self):
         self.X_one = [[1, 2]]
         self.X_two = [[1, 0], [1, 3]]
-        self.Gram_matrix_degree_one_x_one_x_one = [[5.]]
-        self.Gram_matrix_degree_two_x_one_x_one = [[25.]]
-        self.Gram_matrix_degree_one_bias_one_x_one_x_one = [[6.]]
-        self.Gram_matrix_normalized_x_one_x_one = [[1.]]
-        self.Gram_matrix_normalized_x_one_x_two = [[0.447213595, 0.989949494]]
+        self.gram_matrix_degree_one_x_one_x_one = [[5.]]
+        self.gram_matrix_degree_two_x_one_x_one = [[25.]]
+        self.gram_matrix_degree_one_bias_one_x_one_x_one = [[6.]]
+        self.gram_matrix_normalized_x_one_x_one = [[1.]]
+        self.gram_matrix_normalized_x_one_x_two = [[0.447213595, 0.989949494]]
 
     def test_degree_one_x_one_x_one_polynomial_kernel_returns_expected_value(self):
         kernel = PolynomialKernel(degree=1, bias=0, is_normalized=False)
 
-        Gram_matrix = kernel(self.X_one, self.X_one)
+        gram_matrix = kernel(self.X_one, self.X_one)
 
-        numpy.testing.assert_array_equal(Gram_matrix, self.Gram_matrix_degree_one_x_one_x_one)
+        numpy.testing.assert_array_equal(gram_matrix, self.gram_matrix_degree_one_x_one_x_one)
 
     def test_degree_two_x_one_x_one_polynomial_kernel_returns_expected_value(self):
         kernel = PolynomialKernel(degree=2, bias=0, is_normalized=False)
 
-        Gram_matrix = kernel(self.X_one, self.X_one)
+        gram_matrix = kernel(self.X_one, self.X_one)
 
-        numpy.testing.assert_array_equal(Gram_matrix, self.Gram_matrix_degree_two_x_one_x_one)
+        numpy.testing.assert_array_equal(gram_matrix, self.gram_matrix_degree_two_x_one_x_one)
 
     def test_degree_one_bias_one_x_one_x_one_polynomial_kernel_returns_expected_value(self):
         kernel = PolynomialKernel(degree=1, bias=1, is_normalized=False)
 
-        Gram_matrix = kernel(self.X_one, self.X_one)
+        gram_matrix = kernel(self.X_one, self.X_one)
 
-        numpy.testing.assert_array_equal(Gram_matrix, self.Gram_matrix_degree_one_bias_one_x_one_x_one)
+        numpy.testing.assert_array_equal(gram_matrix, self.gram_matrix_degree_one_bias_one_x_one_x_one)
 
     def test_x_one_x_one_normalized_polynomial_kernel_returns_expected_value(self):
         kernel = PolynomialKernel(degree=1, bias=0, is_normalized=True)
 
-        Gram_matrix = kernel(self.X_one, self.X_one)
+        gram_matrix = kernel(self.X_one, self.X_one)
 
-        numpy.testing.assert_array_equal(Gram_matrix, self.Gram_matrix_normalized_x_one_x_one)
+        numpy.testing.assert_array_equal(gram_matrix, self.gram_matrix_normalized_x_one_x_one)
 
     def test_degree_one_x_one_x_two_normalized_polynomial_kernel_returns_expected_value(self):
         kernel = PolynomialKernel(degree=1, bias=0, is_normalized=True)
 
-        Gram_matrix = kernel(self.X_one, self.X_two)
+        gram_matrix = kernel(self.X_one, self.X_two)
 
-        numpy.testing.assert_almost_equal(Gram_matrix, self.Gram_matrix_normalized_x_one_x_two)
+        numpy.testing.assert_almost_equal(gram_matrix, self.gram_matrix_normalized_x_one_x_two)
 
 
 if __name__ == '__main__':
