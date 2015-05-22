6b77958948a61058a75537234497589aed0de14b
Amelie <amelie.rolland.1@gmail.com>
2015-05-21 23:34:06 -0400

    refactor tests

--- preimage/tests/metrics/test_structured_output.py
+++ preimage/tests/metrics/test_structured_output.py
@@ -19,6 +19,8 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
         self.unicor_potato = self.unicor + self.potato
         self.unicarn_tomato = self.unicarn + self.tomato
         self.nicornu_tomato = self.nicornu + self.tomato
+        self.hamming_loss_unicarn_potato = (1. / 7 + 2. / 6) / 2
+        self.leveinshtein_loss_nicornu_tomato = (2. / 7 + 2. / 6) / 2
 
     def test_same_word_zero_one_loss_is_zero(self):
         loss = zero_one_loss(self.unicorn, self.unicorn)
@@ -47,7 +49,7 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
     def test_one_wrong_letter_on_seven_hamming_loss_is_one_on_seven(self):
         loss = hamming_loss(self.unicorn, self.unicarn)
 
-        self.assertEqual(loss, 1./7)
+        self.assertEqual(loss, 1. / 7)
 
     def test_all_wrong_letters_hamming_loss_is_one(self):
         loss = hamming_loss(self.unicorn, self.nicornu)
@@ -56,9 +58,8 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
 
     def test_two_words_with_errors_hamming_loss_is_mean_of_errors(self):
         loss = hamming_loss(self.unicorn_potato, self.unicarn_tomato)
-        mean_errors = (1./7 + 2./6) / 2
 
-        self.assertEqual(loss, mean_errors)
+        self.assertEqual(loss, self.hamming_loss_unicarn_potato)
 
     def test_not_same_word_count_hamming_loss_throws_value_error(self):
         with self.assertRaises(ValueError):
@@ -76,12 +77,12 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
     def test_one_missing_letter_on_seven_levenshtein_loss_is_one_on_seven(self):
         loss = levenshtein_loss(self.unicorn, self.unicor)
 
-        self.assertEqual(loss, 1./7)
+        self.assertEqual(loss, 1. / 7)
 
     def test_one_extra_letter_on_seven_levenshtein_loss_is_one_on_seven(self):
         loss = levenshtein_loss(self.unicor, self.unicorn)
 
-        self.assertEqual(loss, 1./7)
+        self.assertEqual(loss, 1. / 7)
 
     def test_no_letters_in_common_levenshtein_loss_is_one(self):
         loss = levenshtein_loss(self.unicorn, self.a)
@@ -90,9 +91,8 @@ class TestStructuredOutputMetrics(unittest2.TestCase):
 
     def test_two_words_with_errors_levenshtein_loss_is_mean_of_errors(self):
         loss = levenshtein_loss(self.unicorn_potato, self.nicornu_tomato)
-        mean_errors = (2./7 + 2./6) / 2
 
-        self.assertEqual(loss, mean_errors)
+        self.assertEqual(loss, self.leveinshtein_loss_nicornu_tomato)
 
     def test_not_same_word_count_levenshtein_loss_throws_value_error(self):
         with self.assertRaises(ValueError):
