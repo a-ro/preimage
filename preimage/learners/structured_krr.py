c58ba04f0f990cb31cc1d8278823eee987220822
Amelie <amelie.rolland.1@gmail.com>
2015-05-22 14:03:36 -0400

    remove base class rename parameters and refactor tests

--- preimage/learners/structured_krr.py
+++ preimage/learners/structured_krr.py
@@ -1,39 +1,47 @@
 __author__ = 'amelie'
 
 import numpy
+from scipy import linalg
+from sklearn.base import BaseEstimator
 
-from preimage.learners.base_krr import KernelRidgeRegression
 
-
-class StructuredKernelRidgeRegression(KernelRidgeRegression):
+class StructuredKernelRidgeRegression(BaseEstimator):
     def __init__(self, alpha, kernel, inference_model):
-        super().__init__(alpha, kernel)
+        self.alpha = alpha
+        self.kernel = kernel
         self.inference_model = inference_model
-        self.Weights_ = None
+        self.weights_ = None
         self.X_train_ = None
 
     def fit(self, X, Y, y_lengths=None):
-        Gram_matrix = self.kernel(X, X)
-        self.Weights_ = self._solve(Gram_matrix)
+        gram_matrix = self.kernel(X, X)
+        self.weights_ = self._solve(gram_matrix)
         self.X_train_ = X
-        inference_parameters = InferenceFitParameters(self.Weights_, Gram_matrix, Y, y_lengths)
+        inference_parameters = InferenceFitParameters(self.weights_, gram_matrix, Y, y_lengths)
         self.inference_model.fit(inference_parameters)
         return self
 
     def predict(self, X, y_lengths=None):
-        if self.Weights_ is None:
+        if self.weights_ is None:
             raise ValueError("The fit function must be called before predict")
-        Gram_matrix = self.kernel(self.X_train_, X)
-        Y_weights = numpy.dot(self.Weights_, Gram_matrix).T
+        gram_matrix = self.kernel(self.X_train_, X)
+        Y_weights = numpy.dot(self.weights_, gram_matrix).T
         Y_predicted = self.inference_model.predict(Y_weights, y_lengths)
         return Y_predicted
+
+    def _solve(self, gram_matrix):
+        diagonal = numpy.copy(gram_matrix.diagonal())
+        numpy.fill_diagonal(gram_matrix, diagonal + self.alpha)
+        weights = linalg.inv(gram_matrix)
+        numpy.fill_diagonal(gram_matrix, diagonal)
+        return weights
 
 
 # Basic structure for parameters inference_model.fit function.
 # That way inference_models.fit(parameters) don't have unused parameters but only access the ones they need
 class InferenceFitParameters:
-    def __init__(self, Weights, Gram_matrix, Y, y_lengths):
-        self.Weights = Weights
-        self.Gram_matrix = Gram_matrix
+    def __init__(self, weights, gram_matrix, Y, y_lengths):
+        self.weights = weights
+        self.gram_matrix = gram_matrix
         self.Y_train = Y
         self.y_lengths = y_lengths
--- preimage/tests/learners/test_structured_krr.py
+++ preimage/tests/learners/test_structured_krr.py
@@ -10,18 +10,28 @@ from preimage.learners.structured_krr import StructuredKernelRidgeRegression, In
 
 class TestStructuredKernelRidgeRegression(unittest2.TestCase):
     def setUp(self):
+        self.setup_x_and_y()
+        self.setup_kernel()
+        self.setup_learners()
+
+    def setup_x_and_y(self):
         self.X_train = [[1., 0], [0.5, 1]]
         self.X_test = [[0, 0], [1, 0], [0, 1]]
         self.Y_train = ['abc', 'ba']
         self.Y_test = ['ab', 'baa', 'a']
         self.y_test_lengths = [2, 3, 1]
         self.y_train_lengths = [1, 2]
-        self.Gram_matrix = numpy.array([[1., 0.5], [0.5, 1.]])
-        self.Gram_matrix_inverse = [[1.33333333, -0.66666667], [-0.66666667, 1.33333333]]
-        self.Gram_matrix_plus_one_half_diagonal_inverse = [[0.75, -0.25], [-0.25, 0.75]]
-        self.Gram_matrix_x_train_x_test = [[0, 1, 0], [0, 0, 1]]
+
+    def setup_kernel(self):
+        self.gram_matrix = numpy.array([[1., 0.5], [0.5, 1.]])
+        self.gram_matrix_copy = numpy.array([[1., 0.5], [0.5, 1.]])
+        self.gram_matrix_inverse = [[1.33333333, -0.66666667], [-0.66666667, 1.33333333]]
+        self.gram_matrix_plus_one_half_diagonal_inverse = [[0.75, -0.25], [-0.25, 0.75]]
+        self.gram_matrix_x_train_x_test = [[0, 1, 0], [0, 0, 1]]
         self.Y_weights = [[0, 0], [1.33333333, -0.66666667], [-0.66666667, 1.33333333]]
-        self.kernel_mock = Mock(side_effect=[self.Gram_matrix, self.Gram_matrix_x_train_x_test])
+        self.kernel_mock = Mock(side_effect=[self.gram_matrix, self.gram_matrix_x_train_x_test])
+
+    def setup_learners(self):
         self.model_mock = Mock()
         self.structured_krr = StructuredKernelRidgeRegression(alpha=0, kernel=self.kernel_mock,
                                                               inference_model=self.model_mock)
@@ -29,7 +39,7 @@ class TestStructuredKernelRidgeRegression(unittest2.TestCase):
     def test_alpha_zero_structured_krr_fit_weights_is_gram_matrix_inverse(self):
         self.structured_krr.fit(self.X_train, self.Y_train)
 
-        numpy.testing.assert_almost_equal(self.structured_krr.Weights_, self.Gram_matrix_inverse)
+        numpy.testing.assert_almost_equal(self.structured_krr.weights_, self.gram_matrix_inverse)
 
     def test_alpha_one_half_structured_krr_fit_weights_is_inverse_of_gram_matrix_plus_one_half_diagonal(self):
         structured_krr = StructuredKernelRidgeRegression(alpha=0.5, kernel=self.kernel_mock,
@@ -37,7 +47,7 @@ class TestStructuredKernelRidgeRegression(unittest2.TestCase):
 
         structured_krr.fit(self.X_train, self.Y_train)
 
-        numpy.testing.assert_almost_equal(structured_krr.Weights_, self.Gram_matrix_plus_one_half_diagonal_inverse)
+        numpy.testing.assert_almost_equal(structured_krr.weights_, self.gram_matrix_plus_one_half_diagonal_inverse)
 
     def test_structured_krr_fit_x_train_is_equal_to_x(self):
         self.structured_krr.fit(self.X_train, self.Y_train)
@@ -50,8 +60,16 @@ class TestStructuredKernelRidgeRegression(unittest2.TestCase):
 
         self.structured_krr.fit(self.X_train, self.Y_train, self.y_train_lengths)
 
-        inference_parameter_mock.assert_called_once_with(self.structured_krr.Weights_, self.Gram_matrix, self.Y_train,
+        inference_parameter_mock.assert_called_once_with(self.structured_krr.weights_, self.gram_matrix, self.Y_train,
                                                          self.y_train_lengths)
+
+    def test_structured_krr_solve_does_not_change_gram_matrix(self):
+        structured_krr = StructuredKernelRidgeRegression(alpha=0.5, kernel=self.kernel_mock,
+                                                         inference_model=self.model_mock)
+
+        structured_krr.fit(self.X_train, self.Y_train, self.y_train_lengths)
+
+        numpy.testing.assert_array_equal(self.gram_matrix, self.gram_matrix_copy)
 
     def test_structured_krr_fit_calls_inference_model_fit(self):
         self.structured_krr.fit(self.X_train, self.Y_train)
@@ -72,7 +90,7 @@ class TestStructuredKernelRidgeRegression(unittest2.TestCase):
 
     def test_structured_krr_predict_sends_y_weights_to_inference_predict(self):
         self.structured_krr.fit(self.X_train, self.Y_train)
-        self.kernel_mock = Mock(return_value=self.Gram_matrix_x_train_x_test)
+        self.kernel_mock = Mock(return_value=self.gram_matrix_x_train_x_test)
 
         self.structured_krr.predict(self.X_test)
 
@@ -80,7 +98,7 @@ class TestStructuredKernelRidgeRegression(unittest2.TestCase):
 
     def test_structured_krr_predict_sends_y_lengths_to_inference_predict(self):
         self.structured_krr.fit(self.X_train, self.Y_train)
-        self.kernel_mock = Mock(return_value=self.Gram_matrix_x_train_x_test)
+        self.kernel_mock = Mock(return_value=self.gram_matrix_x_train_x_test)
 
         self.structured_krr.predict(self.X_test, self.y_test_lengths)
 
