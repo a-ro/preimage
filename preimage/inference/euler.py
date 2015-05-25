11c058028d8971f52e6d114e93595cdbd98456a1
Amelie <amelie.rolland.1@gmail.com>
2015-05-24 22:07:52 -0400

    refactor + add parameter validations

--- preimage/inference/euler.py
+++ preimage/inference/euler.py
@@ -5,22 +5,26 @@ from copy import deepcopy
 import numpy
 
 from preimage.utils.alphabet import get_index_to_n_gram
+from preimage.exceptions.n_gram import InvalidNGramLengthError, InvalidYLengthError, NoThresholdsError
+from preimage.exceptions.shape import InvalidShapeError
 
 
 class EulerianPath:
-    def __init__(self, alphabet, n, min_length, is_merging_path=False):
-        self.alphabet = alphabet
-        self.n = int(n)
-        self.is_merging_path = is_merging_path
-        self.min_n_gram_count = int(n) if min_length is None else min_length - self.n + 1
-        self.index_to_n_gram = get_index_to_n_gram(alphabet, self.n)
+    def __init__(self, alphabet, n, min_length=1, is_merging_path=True):
+        self._n = int(n)
+        self._is_merging_path = is_merging_path
+        self._min_n_gram_count = 1 if min_length <= self._n else min_length - self._n + 1
+        self._index_to_n_gram = get_index_to_n_gram(alphabet, self._n)
+        self._n_gram_count = len(alphabet) ** self._n
+        self._verify_n(self._n)
 
-    def find_eulerian_path(self, N_gram_weights, y_length=None, thresholds=None):
-        if y_length is None:
-            rounded_weights = self._round_weights_with_thresholds(N_gram_weights, thresholds)
-        else:
-            n_gram_count = y_length - self.n + 1
-            rounded_weights = self.round_weights_to_n_gram_count(N_gram_weights, n_gram_count)
+    def _verify_n(self, n):
+        if n <= 1:
+            raise InvalidNGramLengthError(n, 1)
+
+    def find_eulerian_path(self, n_gram_weights, y_length=None, thresholds=None):
+        self._verify_weights_length_thresholds(n_gram_weights, y_length, thresholds)
+        rounded_weights = self._round_weights(n_gram_weights, thresholds, y_length)
         n_gram_indexes = numpy.where(rounded_weights > 0)[0]
         n_grams = self._get_n_grams_in_selected_indexes(n_gram_indexes, rounded_weights)
         y = self._find_y_corresponding_to_n_grams(n_grams)
@@ -28,101 +32,114 @@ class EulerianPath:
             y = y[0:y_length]
         return y
 
-    def _round_weights_with_thresholds(self, weights, thresholds):
-        rounded_weights = numpy.asarray(weights > thresholds, dtype=numpy.int)
-        non_zero_weight_count = rounded_weights.sum()
-        # Avoid having zero n gram predicted
-        if non_zero_weight_count < self.min_n_gram_count:
-            best_weights_index = numpy.argpartition(-weights, self.min_n_gram_count)
-            best_weights_index = best_weights_index[rounded_weights[best_weights_index] == 0]
-            rounded_weights[best_weights_index[0:self.min_n_gram_count - non_zero_weight_count]] = 1
+    def _verify_weights_length_thresholds(self, n_gram_weights, y_length, thresholds):
+        if n_gram_weights.shape[0] != self._n_gram_count:
+            raise InvalidShapeError('n_gram_weights', n_gram_weights.shape, [(self._n_gram_count,)])
+        if y_length is not None and y_length < self._n:
+            raise InvalidYLengthError(self._n, y_length)
+        if thresholds is not None and thresholds.shape[0] != self._n_gram_count:
+            raise InvalidShapeError('thresholds', thresholds.shape, [(self._n_gram_count,)])
+        if thresholds is None and y_length is None:
+            raise NoThresholdsError()
+
+    def _round_weights(self, n_gram_weights, thresholds, y_length):
+        if y_length is None:
+            rounded_weights = self._round_weights_with_thresholds(n_gram_weights, thresholds)
+        else:
+            n_gram_count = y_length - self._n + 1
+            rounded_weights = self._round_weights_to_n_gram_count(n_gram_weights, n_gram_count)
         return rounded_weights
 
-    # Didn't check cases where weights are all zero, all negative, or all the same
-    # The case when the rounded_n_gram_count > n_gram_count_in_y didn't happen for ocr-letter
-    # In cases where some weight values are equal, the rounding might give rounded_n_gram_count > n_gram_count_in_y
-    # In that case, we computed a euler tour with all the predicted n-grams and cut to get the right length after
-    def round_weights_to_n_gram_count(self, weights, n_gram_count_in_y):
+    def _round_weights_to_n_gram_count(self, weights, n_gram_count_in_y):
         weights_copy = numpy.copy(weights)
         weights_copy[weights_copy < 0] = 0.
         rounded_weights = numpy.round(weights_copy)
-        rounded_n_gram_count = rounded_weights.sum()
         positive_weights = weights_copy[weights > 0]
-        if rounded_n_gram_count < n_gram_count_in_y:
+        if rounded_weights.sum() < n_gram_count_in_y:
             rounded_weights = self._add_n_grams_to_rounded_sum(weights_copy, positive_weights, n_gram_count_in_y)
         return rounded_weights
 
     def _add_n_grams_to_rounded_sum(self, weights_copy, positive_weights, n_gram_count_in_y):
-        while (numpy.round(weights_copy).sum() < n_gram_count_in_y):
+        while numpy.round(weights_copy).sum() < n_gram_count_in_y:
             multiplicative_factors = (numpy.ceil(positive_weights + 0.5) - 0.49) / positive_weights
             min_factor = numpy.min(multiplicative_factors[multiplicative_factors > 1.])
             weights_copy = min_factor * weights_copy
             positive_weights = min_factor * positive_weights
         return numpy.round(weights_copy)
 
+    def _round_weights_with_thresholds(self, weights, thresholds):
+        rounded_weights = numpy.asarray(weights > thresholds, dtype=numpy.int)
+        non_zero_weight_count = rounded_weights.sum()
+        # Avoid having zero n gram predicted
+        if non_zero_weight_count < self._min_n_gram_count:
+            best_weight_indexes = numpy.argpartition(-weights, self._min_n_gram_count)[0:self._min_n_gram_count]
+            best_zero_weight_indexes = best_weight_indexes[rounded_weights[best_weight_indexes] == 0]
+            rounded_weights[best_zero_weight_indexes[0:self._min_n_gram_count - non_zero_weight_count]] = 1
+        return rounded_weights
+
     def _get_n_grams_in_selected_indexes(self, selected_n_gram_indexes, rounded_weights):
-        repeated_n_grams = [self.index_to_n_gram[index] for index in selected_n_gram_indexes
+        repeated_n_grams = [self._index_to_n_gram[index] for index in selected_n_gram_indexes
                             for _ in range(int(rounded_weights[index]))]
         return numpy.array(repeated_n_grams)
 
     def _find_y_corresponding_to_n_grams(self, n_grams):
-        y = ""
-        nodes, leaving_edges, marked_edges = self._get_nodes_and_leaving_edges(n_grams)
-        is_first_path = True
-        if self.is_merging_path:
-            while len(nodes) > 0:
-                best_path, marked_edges = self._find_best_path(nodes, leaving_edges, marked_edges, n_grams)
-                y += self._transform_path_in_word(best_path, is_first_path)
-                is_first_path = False
-                nodes = [node for node, edges in marked_edges.items() if sum(edges) < len(edges)]
+        nodes, leaving_edges, marked_edges = self._get_nodes_and_edges(n_grams)
+        if self._is_merging_path:
+            path = self._merge_best_paths(nodes, leaving_edges, marked_edges, n_grams)
         else:
-            best_path, marked_edges = self._find_best_path(nodes, leaving_edges, marked_edges, n_grams)
-            y = self._transform_path_in_word(best_path, is_first_path)
+            path, marked_edges = self._find_best_path(nodes, leaving_edges, marked_edges, n_grams)
+        y = self._transform_path_in_word(path)
         return y
 
-    def _get_nodes_and_leaving_edges(self, n_grams):
-        nodes = numpy.unique([n_gram[j:j + self.n - 1] for n_gram in n_grams for j in range(2)])
-        permutations = numpy.random.permutation(nodes.shape[0])
-        nodes = nodes[permutations]
-        permutations = numpy.random.permutation(n_grams.shape[0])
-        random_n_grams = deepcopy(n_grams)[permutations]
+    def _get_nodes_and_edges(self, n_grams):
+        nodes = numpy.unique([n_gram[j:j + self._n - 1] for n_gram in n_grams for j in range(2)])
+        nodes = nodes[numpy.random.permutation(nodes.shape[0])]
+        random_n_grams = n_grams[numpy.random.permutation(n_grams.shape[0])]
         leaving_edges = {node: [] for node in nodes}
         marked_edges = {node: [] for node in nodes}
-        for n_gram in random_n_grams:
-            leaving_edges[n_gram[0:self.n - 1]].append(n_gram[1:])
-            marked_edges[n_gram[0:self.n - 1]].append(False)
+        self._update_leaving_and_marked_edges(leaving_edges, marked_edges, random_n_grams)
         return nodes, leaving_edges, marked_edges
+
+    def _update_leaving_and_marked_edges(self, leaving_edges, marked_edges, random_n_grams):
+        for n_gram in random_n_grams:
+            leaving_edges[n_gram[0:self._n - 1]].append(n_gram[1:])
+            marked_edges[n_gram[0:self._n - 1]].append(False)
+
+    def _merge_best_paths(self, nodes, leaving_edges, marked_edges, n_grams):
+        path = []
+        while len(nodes) > 0:
+            best_path, marked_edges = self._find_best_path(nodes, leaving_edges, marked_edges, n_grams)
+            path += best_path
+            nodes = [node for node, edges in marked_edges.items() if sum(edges) < len(edges)]
+        return path
+
+    def _find_best_path(self, nodes, leaving_edges, marked_edges, n_grams):
+        best_path = []
+        best_marked_edges = {}
+        for node in nodes:
+            path_marked_edges = deepcopy(marked_edges)
+            path = [node] + self._euler(node, leaving_edges, path_marked_edges)
+            best_path, best_marked_edges = self._update_best_path(path, path_marked_edges, best_path, best_marked_edges)
+            if len(path) == len(n_grams) + 1:
+                break
+        return best_path, best_marked_edges
 
     def _euler(self, node, leaving_edges, marked_edges):
         path = []
         for edge_index, destination_node in enumerate(leaving_edges[node]):
             if not marked_edges[node][edge_index]:
                 marked_edges[node][edge_index] = True
-                old_path = deepcopy(path)
-                path = [destination_node] + self._euler(destination_node, leaving_edges, marked_edges) + old_path
+                path = [destination_node] + self._euler(destination_node, leaving_edges, marked_edges) + path
         return path
 
-    def _find_best_path(self, nodes, leaving_edges, marked_edges, n_grams):
-        best_path = []
-        best_path_marked_edges = {}
-        best_path_length = 0
-        for node in nodes:
-            path_marked_edges = deepcopy(marked_edges)
-            path = [node] + self._euler(node, leaving_edges, path_marked_edges)
-            if len(path) >= best_path_length:
-                best_path = path
-                best_path_marked_edges = deepcopy(path_marked_edges)
-                if len(path) == len(n_grams) + 1:
-                    break
-                else:
-                    best_path_length = len(path)
-        return best_path, best_path_marked_edges
+    def _update_best_path(self, path, path_marked_edges, best_path, best_marked_edges):
+        if len(path) >= len(best_path):
+            best_path = path
+            best_marked_edges = deepcopy(path_marked_edges)
+        return best_path, best_marked_edges
 
-    def _transform_path_in_word(self, path, is_first_path):
-        if is_first_path:
-            y = path[0]
-        else:
-            y = ""
+    def _transform_path_in_word(self, path):
+        y = path[0]
         for node in path[1:]:
             y += node[-1:]
         return y
--- preimage/tests/inference/test_euler.py
+++ preimage/tests/inference/test_euler.py
@@ -2,15 +2,37 @@ __author__ = 'amelie'
 
 import unittest2
 import numpy.testing
+from mock import patch
 
 from preimage.inference.euler import EulerianPath
+from preimage.exceptions.n_gram import InvalidNGramLengthError, InvalidYLengthError, NoThresholdsError
+from preimage.exceptions.shape import InvalidShapeError
 
 
-class TestEuler(unittest2.TestCase):
+class TestEulerianPath(unittest2.TestCase):
     def setUp(self):
         numpy.random.seed(42)
+        self.setup_alphabet()
+        self.setup_weights()
+        self.setup_thresholds()
+        self.eulerian_path_algorithm = EulerianPath(self.alphabet, n=2, min_length=2, is_merging_path=False)
+
+    def setup_alphabet(self):
         self.alphabet = ['a', 'b', 'c']
         self.small_alphabet = ['a', 'b']
+        self.aabc_aacb = ['aabc', 'aacb']
+        self.cac_aca = ['cac', 'aca']
+        self.abba_bbab_babb = ['abba', 'bbab', 'babb']
+        self.ccbc_cbcc_bccb = ['ccbc', 'cbcc', 'bccb']
+        self.ccbcaa_cbccaa_bccbaa = ['ccbcaa', 'cbccaa', 'bccbaa']
+        self.aaaabb_bbbbaa = ['aaaabb', 'bbbbaa']
+        self.index_to_n_gram_patch = patch('preimage.inference.euler.get_index_to_n_gram')
+        self.index_to_two_gram = {0: 'aa', 1: 'ab', 2: 'ac', 3: 'ba', 4: 'bb', 5: 'bc', 6: 'ca', 7: 'cb', 8: 'cc'}
+        self.index_to_three_gram = {0: 'aaa', 1: 'aab', 2: 'aba', 3: 'abb', 4: 'baa', 5: 'bab', 6: 'bba', 7: 'bbb'}
+        self.index_to_n_gram_patch.start().return_value = self.index_to_two_gram
+
+    def setup_weights(self):
+        self.a_b_c_weights = numpy.array([1., 1., 1.])
         self.ab_weights = numpy.array([0, 1., 0, 0, 0, 0, 0, 0, 0])
         self.ab_ca_weights = numpy.array([0, 1., 0, 0, 0, 0, 1., 0, 0])
         self.ab_ba_ca_weights = numpy.array([0, 1., 0, 1., 0, 0, 1., 0, 0])
@@ -21,20 +43,16 @@ class TestEuler(unittest2.TestCase):
         self.aab_weights = numpy.array([0, 1., 0, 0., 0, 0, 0, 0])
         self.aab_aab_weights = numpy.array([0, 2., 0, 0., 0, 0, 0, 0])
         self.aaa_bbb_weights = numpy.array([2., 0., 0, 0., 0, 0, 0, 2.])
-        self.ac_threshold_weights = numpy.array([0, 1., 4., 2., 0, 0, 3., 1., 0])
-        self.aa_ac_threshold_weights = numpy.array([1.5, 1., 4., 2., 0, 0, 1., 1., 0])
         self.bc_cc_non_rounded_weights = numpy.array([0, 0, 0, 0, 0, 0.6, 0, 0, 1.5])
         self.bb_bc_non_rounded_weights = numpy.array([0, 0, 0, 0, 0.4, 0.6, 0, 0, 0])
         self.bb_bb_non_rounded_weights = numpy.array([0, 0, 0, 0, 0.6, 0.1, 0, 0, 0])
+
+    def setup_thresholds(self):
         self.thresholds = numpy.arange(1, 10)
         self.high_tresholds = numpy.arange(5, 14)
-        self.aabc_aacb = ['aabc', 'aacb']
-        self.cac_aca = ['cac', 'aca']
-        self.abba_bbab_babb = ['abba', 'bbab', 'babb']
-        self.ccbc_cbcc_bccb = ['ccbc', 'cbcc', 'bccb']
-        self.ccbcaa_cbccaa_bccbaa = ['ccbcaa', 'cbccaa', 'bccbaa']
-        self.aaaabb_bbbbaa = ['aaaabb', 'bbbbaa']
-        self.eulerian_path_algorithm = EulerianPath(self.alphabet, n=2, min_length=2, is_merging_path=False)
+        self.wrong_thresholds = numpy.array([1, 2, 3])
+        self.ac_threshold_weights = numpy.array([0, 1., 4., 2., 0, 0, 3., 1., 0])
+        self.aa_ac_threshold_weights = numpy.array([1.5, 1., 4., 2., 0, 0, 1., 1., 0])
 
     def test_one_n_gram_find_eulerian_path_returns_n_gram(self):
         y = self.eulerian_path_algorithm.find_eulerian_path(self.ab_weights, y_length=2)
@@ -75,11 +93,6 @@ class TestEuler(unittest2.TestCase):
 
         self.assertIn(y, self.abba_bbab_babb)
 
-    def test_eulerian_cycle_three_n_gram_find_eulerian_path_returns_eulerian_cycle(self):
-        y = self.eulerian_path_algorithm.find_eulerian_path(self.ab_ba_bb_weights, y_length=4)
-
-        self.assertIn(y, self.abba_bbab_babb)
-
     def test_merge_path_eulerian_cycle_n_gram_find_eulerian_path_returns_eulerian_cycle(self):
         self.eulerian_path_algorithm = EulerianPath(self.alphabet, n=2, min_length=2, is_merging_path=True)
 
@@ -105,6 +118,7 @@ class TestEuler(unittest2.TestCase):
         self.assertIn(y, self.aabc_aacb)
 
     def test_one_three_gram_find_eulerian_path_returns_three_gram(self):
+        self.index_to_n_gram_patch.start().return_value = self.index_to_three_gram
         eulerian_path_algorithm = EulerianPath(self.small_alphabet, n=3, min_length=3, is_merging_path=False)
 
         y = eulerian_path_algorithm.find_eulerian_path(self.aab_weights, y_length=3)
@@ -112,6 +126,7 @@ class TestEuler(unittest2.TestCase):
         self.assertEqual(y, 'aab')
 
     def test_two_three_gram_conntected_find_eulerian_path_merges_two_three_grams(self):
+        self.index_to_n_gram_patch.start().return_value = self.index_to_three_gram
         eulerian_path_algorithm = EulerianPath(self.small_alphabet, n=3, min_length=3, is_merging_path=False)
 
         y = eulerian_path_algorithm.find_eulerian_path(self.aab_aab_weights, y_length=4)
@@ -119,6 +134,7 @@ class TestEuler(unittest2.TestCase):
         self.assertEqual(y, 'aabb')
 
     def test_merge_path_two_three_gram_cycles_find_eulerian_path_merges_two_cycles(self):
+        self.index_to_n_gram_patch.start().return_value = self.index_to_three_gram
         eulerian_path_algorithm = EulerianPath(self.small_alphabet, n=3, min_length=3, is_merging_path=True)
 
         y = eulerian_path_algorithm.find_eulerian_path(self.aaa_bbb_weights, y_length=6)
@@ -128,7 +144,7 @@ class TestEuler(unittest2.TestCase):
     def test_smaller_length_find_eulerian_path_cuts_y_to_fit_length(self):
         y = self.eulerian_path_algorithm.find_eulerian_path(self.ab_ca_weights, y_length=2)
 
-        self.assertEqual(y, 'ca')
+        self.assertIn(y, ['ab', 'ca'])
 
     def test_one_weight_above_threshold_find_eulerian_path_returns_one_n_gram(self):
         y = self.eulerian_path_algorithm.find_eulerian_path(self.ac_threshold_weights, thresholds=self.thresholds)
@@ -159,20 +175,40 @@ class TestEuler(unittest2.TestCase):
 
         self.assertIn(y, self.cac_aca)
 
-    def test_round_weights_is_exact_length_find_eulerian_path_returns_expected_path(self):
+    def test_rounded_weights_is_exact_length_find_eulerian_path_returns_expected_path(self):
         y = self.eulerian_path_algorithm.find_eulerian_path(self.bc_cc_non_rounded_weights, y_length=4)
 
-        self.assertIn(y, 'bccc')
+        self.assertEqual(y, 'bccc')
 
     def test_rounded_weights_miss_one_weight_find_eulerian_path_returns_expected_path(self):
         y = self.eulerian_path_algorithm.find_eulerian_path(self.bb_bc_non_rounded_weights, y_length=3)
 
-        self.assertIn(y, 'bbc')
+        self.assertEqual(y, 'bbc')
 
     def test_rounded_weights_miss_one_weight_find_eulerian_path_repeats_best_n_gram(self):
         y = self.eulerian_path_algorithm.find_eulerian_path(self.bb_bb_non_rounded_weights, y_length=3)
 
-        self.assertIn(y, 'bbb')
+        self.assertEqual(y, 'bbb')
+
+    def test_one_gram_eulerian_path_algorithm_raises_n_gram_length_error(self):
+        with self.assertRaises(InvalidNGramLengthError):
+            EulerianPath(self.alphabet, n=1)
+
+    def test_y_length_smaller_than_n_find_eulerian_path_raises_length_error(self):
+        with self.assertRaises(InvalidYLengthError):
+            self.eulerian_path_algorithm.find_eulerian_path(self.ab_ba_bb_weights, y_length=1)
+
+    def test_n_gram_weights_wrong_shape_find_eulerian_path_raises_shape_error(self):
+        with self.assertRaises(InvalidShapeError):
+            self.eulerian_path_algorithm.find_eulerian_path(self.a_b_c_weights, y_length=2)
+
+    def test_no_length_no_thresholds_find_eulerian_path_raises_threshold_error(self):
+        with self.assertRaises(NoThresholdsError):
+            self.eulerian_path_algorithm.find_eulerian_path(self.ab_ba_bb_weights, y_length=None, thresholds=None)
+
+    def test_thresholds_wrong_shape_find_eulerian_path_raises_shape_error(self):
+        with self.assertRaises(InvalidShapeError):
+            self.eulerian_path_algorithm.find_eulerian_path(self.ab_ba_bb_weights, thresholds=self.wrong_thresholds)
 
 
 if __name__ == '__main__':
