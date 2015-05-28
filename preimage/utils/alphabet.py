baa469aa123118bcb093357cc0647691e4d14dce
Amelie <amelie.rolland.1@gmail.com>
2015-05-28 13:45:40 -0400

    modify alphabet to list instead of array (the branch and bound is expecting a list)

--- preimage/utils/alphabet.py
+++ preimage/utils/alphabet.py
@@ -8,8 +8,8 @@ from preimage.exceptions.n_gram import InvalidNGramLengthError
 
 
 class Alphabet:
-    latin = numpy.array(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-                         'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+    latin = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+             'v', 'w', 'x', 'y', 'z']
 
 
 def get_n_gram_to_index(alphabet, n):
