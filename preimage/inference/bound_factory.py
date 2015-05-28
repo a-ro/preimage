0c9f111ca3c53d7ce1d7f709df76f529a26dc18f
Amelie <amelie.rolland.1@gmail.com>
2015-05-28 13:52:43 -0400

    add bound factory for the n-gram and gs kernel (sigma_position) bound calculators

---
+++ preimage/inference/bound_factory.py
@@ -0,0 +1,19 @@
+__author__ = 'amelie'
+
+import numpy
+from preimage.inference._bound_calculator import OCRMinBoundCalculator, MaxBoundCalculator
+from preimage.inference._node_creator import NodeCreator
+from preimage.utils.position import compute_position_weights
+
+def get_n_gram_node_creator(n, graph, graph_weights, y_length, n_gram_to_index, n_grams):
+    max_bound = MaxBoundCalculator(n, graph, graph_weights.reshape(1, -1), n_gram_to_index)
+    min_bound = OCRMinBoundCalculator(n, numpy.ones(y_length), n_grams)
+    node_creator = NodeCreator(min_bound, max_bound, n_grams)
+    return node_creator
+
+def get_gs_node_creator(n, graph, graph_weights, y_length, n_gram_to_index, n_grams, sigma_position):
+    position_weights = compute_position_weights(0, y_length, sigma_position)
+    max_bound = MaxBoundCalculator(n, graph, graph_weights, n_gram_to_index)
+    min_bound = OCRMinBoundCalculator(n, position_weights, n_grams)
+    node_creator = NodeCreator(min_bound, max_bound, n_grams)
+    return node_creator
