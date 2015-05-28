0f365c2ae0ab54568d0226684e5cee69a16b4f4e
Amelie <amelie.rolland.1@gmail.com>
2015-05-28 13:51:06 -0400

    add branch and bound length and no length

---
+++ preimage/inference/_branch_and_bound.pyx
@@ -0,0 +1,119 @@
+# cython: profile=True
+
+import cython
+import heapq
+import numpy
+cimport numpy
+import time
+from preimage.inference._node cimport MaxNode
+from preimage.inference._node_creator cimport NodeCreator
+
+
+ctypedef numpy.float64_t FLOAT64_t
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef branch_and_bound(NodeCreator node_creator, int y_length, list alphabet, float max_time):
+    cdef float start_time = time.clock()
+    cdef float current_time = time.clock() - start_time
+    cdef str empty_string = ""
+    cdef MaxNode node, best_node
+    cdef list heap = node_creator.get_start_nodes(y_length)
+
+    heapq.heapify(heap)
+    best_node = MaxNode(empty_string, -numpy.inf, 0, 0)
+    while(len(heap) > 0 and current_time < max_time):
+        node = heapq.heappop(heap)
+        if  best_node < node:
+            break
+        node = depth_first_search(node, best_node, node_creator, heap, y_length, alphabet)
+        if node < best_node and len(node.y) == y_length:
+            best_node = node
+        current_time = time.clock() - start_time
+    return best_node.y, best_node.get_bound()
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef branch_and_bound_no_length(NodeCreator node_creator, int min_length, int max_length, list alphabet,
+                                 float max_time):
+    cdef float start_time = time.clock()
+    cdef float current_time = time.clock() - start_time
+    cdef str empty_string = ""
+    cdef MaxNode best_node
+    cdef int key, length
+    cdef list heap
+    cdef list keys_to_remove
+    cdef dict heaps
+
+    heaps = get_heap_for_each_length(node_creator, min_length, max_length)
+    best_node = MaxNode(empty_string, -numpy.inf, 0, 0)
+    while(len(heaps) > 0 and current_time < max_time):
+        keys_to_remove = []
+        for length, heap in heaps.items():
+            best_node = find_length_best_node(best_node, node_creator, heap, length, alphabet, keys_to_remove)
+            current_time = time.clock() - start_time
+            if current_time > max_time:
+                break
+        for key in keys_to_remove:
+            heaps.pop(key)
+        current_time = time.clock() - start_time
+    return best_node.y, best_node.get_bound()
+
+
+cdef MaxNode find_length_best_node(MaxNode best_node, NodeCreator node_creator, list heap, int length,
+                                   list alphabet, list keys_to_remove):
+    cdef MaxNode node
+
+    if len(heap) == 0:
+         keys_to_remove.append(length)
+    else:
+        node = heapq.heappop(heap)
+        if best_node < node:
+            keys_to_remove.append(length)
+        else:
+            node = depth_first_search(node, best_node, node_creator, heap, length, alphabet)
+            if node < best_node and len(node.y) == length:
+                best_node = node
+    return best_node
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef MaxNode depth_first_search(MaxNode node, MaxNode best_node, NodeCreator node_creator, list heap,
+                                int y_length, list alphabet):
+    cdef int i
+    cdef MaxNode parent_node, child
+    cdef str letter, child_y
+
+    for i in range(len(node.y), y_length):
+        parent_node = node
+        child_y = alphabet[0] + parent_node.y
+        node = node_creator.create_node(child_y, parent_node, y_length)
+        for letter in alphabet[1:]:
+            child_y = letter + parent_node.y
+            child = node_creator.create_node(child_y, parent_node, y_length)
+            if child < node:
+                if node < best_node:
+                     heapq.heappush(heap, node)
+                node = child
+            else:
+                if(child < best_node):
+                    heapq.heappush(heap, child)
+        if best_node < node:
+            break
+    return node
+
+
+cdef dict get_heap_for_each_length(NodeCreator node_creator, int min_length, int max_length):
+    cdef list heap
+    cdef list keys_to_remove
+    cdef dict heaps = {}
+    cdef int length
+
+    for length in range(min_length, max_length + 1):
+        heap = node_creator.get_start_nodes(length)
+        heapq.heapify(heap)
+        heaps[length] = heap
+    return heaps
---
+++ preimage/tests/inference/test_branch_and_bound.py
@@ -0,0 +1,307 @@
+__author__ = 'amelie'
+
+from mock import MagicMock
+import unittest2
+
+from preimage.inference._branch_and_bound import branch_and_bound, branch_and_bound_no_length
+from preimage.inference._node_creator import NodeCreatorMock
+from preimage.inference._node import MaxNode
+
+
+def bound_side_effect_cb_best(y, final_length):
+    nodes = {'ab': MaxNode('ab', 1., 0, 0.), 'ac': MaxNode('ac', 0., 0, 0.), 'bb': MaxNode('bb', 1., 0, 0.),
+             'bc': MaxNode('bc', 0, 0, 0.), 'cb': MaxNode('cb', 1.5, 0, 0.), 'cc': MaxNode('cc', 0., 0, 0.)}
+    return nodes[y]
+
+
+def bound_side_effect_cc_best(y, final_length):
+    nodes = {'ab': MaxNode('ab', 1., 0, 0.), 'ac': MaxNode('ac', 0., 0, 0.), 'bb': MaxNode('bb', 1., 0, 0.),
+             'bc': MaxNode('bc', 0, 0, 0.), 'cb': MaxNode('cb', 1.5, 0, 0.), 'cc': MaxNode('cc', 1.6, 0, 0.)}
+    return nodes[y]
+
+
+def bound_side_effect_acb_best(y, final_length):
+    nodes = {'ab': MaxNode('ab', 1., 0, 0.), 'bb': MaxNode('bb', 1., 0, 0.), 'cb': MaxNode('cb', 1.5, 0, 0.),
+             'acb': MaxNode('acb', 1.4, 0, 0.), 'bcb': MaxNode('bcb', 1.3, 0, 0.), 'ccb': MaxNode('ccb', 1.3, 0, 0.)}
+    return nodes[y]
+
+
+def bound_side_effect_bbb_best(y, final_length):
+    nodes = {'ab': MaxNode('ab', 1., 0, 0.), 'bb': MaxNode('bb', 1.4, 0, 0.), 'cb': MaxNode('cb', 1.5, 0, 0.),
+             'acb': MaxNode('acb', 1, 0, 0.), 'bcb': MaxNode('bcb', 1.1, 0, 0.), 'ccb': MaxNode('ccb', 1.1, 0, 0.),
+             'abb': MaxNode('abb', 1, 0, 0.), 'bbb': MaxNode('bbb', 1.3, 0, 0.), 'cbb': MaxNode('cbb', 1.1, 0, 0.)}
+    return nodes[y]
+
+
+def bound_side_effect_abb_best_with_two_heap_pop(y, final_length):
+    nodes = {'ab': MaxNode('ab', 1., 0, 0.), 'bb': MaxNode('bb', 1.4, 0, 0.), 'cb': MaxNode('cb', 1.5, 0, 0.),
+             'acb': MaxNode('acb', 1, 0, 0.), 'bcb': MaxNode('bcb', 1.1, 0, 0.), 'ccb': MaxNode('ccb', 1.1, 0, 0.),
+             'abb': MaxNode('abb', 1.3, 0, 0.), 'bbb': MaxNode('bbb', 1, 0, 0.), 'cbb': MaxNode('cbb', 1.1, 0, 0.),
+             'ac': MaxNode('ac', 1., 0, 0.), 'bc': MaxNode('bc', 1, 0, 0.), 'cc': MaxNode('cb', 0, 0, 0.)}
+    return nodes[y]
+
+
+def bound_side_effect_bc_best_multiple_lengths(y, final_length):
+    nodes = {2: {'ab': MaxNode('ab', 1., 0, 0.), 'bb': MaxNode('bb', 1.4, 0, 0.), 'cb': MaxNode('cb', 1.2, 0, 0.),
+                 'ac': MaxNode('ac', 1., 0, 0.), 'bc': MaxNode('bc', 1.5, 0, 0.), 'cc': MaxNode('cc', 1.2, 0, 0.)},
+             3: {'aa': MaxNode('aa', 1.6, 0, 0.), 'ba': MaxNode('ba', 1, 0, 0.), 'ca': MaxNode('ca', 1, 0, 0.),
+                 'aaa': MaxNode('aaa', 1.4, 0, 0.), 'baa': MaxNode('baa', 1, 0, 0.), 'caa': MaxNode('caa', 1, 0, 0.)}}
+    return nodes[final_length][y]
+
+
+def start_node_side_effect_b_length_one_best(final_length):
+    nodes = {1: [MaxNode('a', 0., 0., 0.), MaxNode('b', 2., 0, 0), MaxNode('c', 1., 0, 0)],
+             2: [MaxNode('a', 0., 0, 0.), MaxNode('b', 1., 0, 0), MaxNode('c', 0., 0, 0)]}
+    return nodes[final_length]
+
+
+def start_node_side_effect_b_length_two_best(final_length):
+    nodes = {1: [MaxNode('a', 0., 0, 0), MaxNode('b', 2., 0, 0.), MaxNode('c', 1., 0, 0)],
+             2: [MaxNode('a', 0., 0, 0), MaxNode('b', 3., 0, 0), MaxNode('c', 0., 0, 0)]}
+    return nodes[final_length]
+
+
+def start_node_side_effect_b_length_two_solution_best(final_length):
+    nodes = {1: [MaxNode('a', 0., 0, 0), MaxNode('b', 1., 0, 0), MaxNode('c', 1., 0, 0)],
+             2: [MaxNode('a', 0., 0, 0.), MaxNode('b', 3., 0, 0.), MaxNode('c', 0., 0, 0)]}
+    return nodes[final_length]
+
+
+def start_node_side_effect_start_at_b_length_two(final_length):
+    nodes = {1: [MaxNode('a', 0., 0, 0), MaxNode('b', 1., 0, 0), MaxNode('c', 0.5, 0, 0)],
+             2: [MaxNode('a', 0., 0, 0.), MaxNode('b', 3, 0, 0), MaxNode('c', 2., 0, 0)],
+             3: [MaxNode('a', 1.6, 0, 0), MaxNode('b', 0., 0, 0), MaxNode('c', 0., 0, 0)]}
+    return nodes[final_length]
+
+
+class TestBranchAndBound(unittest2.TestCase):
+    def setUp(self):
+        self.setup_alphabet()
+        self.setup_nodes()
+        self.time = 30
+
+    def setup_alphabet(self):
+        self.alphabet = ['a', 'b', 'c']
+
+    def setup_bound_calculator(self, start_nodes, side_effect=bound_side_effect_cb_best):
+        bound_calculator_mock = MagicMock()
+        bound_calculator_mock.get_start_nodes.return_value = start_nodes
+        bound_calculator_mock.get_start_nodes.return_value = start_nodes
+        bound_calculator_mock.create_node.side_effect = side_effect
+        self.bound_calculator = NodeCreatorMock(bound_calculator_mock)
+
+    def setup_nodes(self):
+        self.b_node_bound = 2
+        self.cb_node_bound = 1.5
+        self.cc_node_bound = 1.6
+        self.acb_node_bound = 1.4
+        self.bbb_node_bound = 1.3
+        self.abb_node_bound = 1.3
+        self.a_b_c_nodes = [MaxNode('a', 0., 1., 0.), MaxNode('b', 2., 1., 2.), MaxNode('c', 1., 1., 1.)]
+        self.a_b_c_nodes_large_c = [MaxNode('a', 0., 0, 0.), MaxNode('b', 2., 0, 0), MaxNode('c', 1.6, 0, 0)]
+
+    def test_one_gram_length_one_branch_and_bound_returns_max_start_node(self):
+        y_length = 1
+        self.setup_bound_calculator(self.a_b_c_nodes)
+
+        y, y_bound = branch_and_bound(self.bound_calculator, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'b')
+        self.assertEqual(y_bound, self.b_node_bound)
+
+    def test_one_gram_length_two_branch_and_bound_returns_best_node(self):
+        y_length = 2
+        self.setup_bound_calculator(self.a_b_c_nodes)
+
+        y, y_bound = branch_and_bound(self.bound_calculator, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'cb')
+        self.assertEqual(y_bound, self.cb_node_bound)
+
+    def test_one_gram_length_two_node_in_heap_better_than_first_solution_branch_and_bound_returns_best_node(self):
+        y_length = 2
+        self.setup_bound_calculator(self.a_b_c_nodes_large_c)
+
+        y, y_bound = branch_and_bound(self.bound_calculator, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'cb')
+        self.assertEqual(y_bound, self.cb_node_bound)
+
+    def test_one_gram_length_two_other_solution_better_than_first_solution_branch_and_bound_returns_best_node(self):
+        y_length = 2
+        self.setup_bound_calculator(self.a_b_c_nodes_large_c, bound_side_effect_cc_best)
+
+        y, y_bound = branch_and_bound(self.bound_calculator, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'cc')
+        self.assertEqual(y_bound, self.cc_node_bound)
+
+    def test_one_gram_length_three_branch_and_bound_returns_best_node(self):
+        y_length = 3
+        self.setup_bound_calculator(self.a_b_c_nodes, bound_side_effect_acb_best)
+
+        y, y_bound = branch_and_bound(self.bound_calculator, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'acb')
+        self.assertEqual(y_bound, self.acb_node_bound)
+
+    def test_one_gram_length_three_other_solution_better_than_first_three_branch_and_bound_returns_best_node(self):
+        y_length = 3
+        self.setup_bound_calculator(self.a_b_c_nodes, bound_side_effect_bbb_best)
+
+        y, y_bound = branch_and_bound(self.bound_calculator, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'bbb')
+        self.assertEqual(y_bound, self.bbb_node_bound)
+
+    def test_one_gram_length_three_other_nodes_better_than_solution_branch_and_bound_returns_best_node(self):
+        y_length = 3
+        self.setup_bound_calculator(self.a_b_c_nodes_large_c, bound_side_effect_abb_best_with_two_heap_pop)
+
+        y, y_bound = branch_and_bound(self.bound_calculator, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'abb')
+        self.assertEqual(y_bound, self.abb_node_bound)
+
+
+class TestBranchAndBoundNoLength(unittest2.TestCase):
+    def setUp(self):
+        self.setup_alphabet()
+        self.setup_nodes()
+        self.time = 30
+
+    def setup_alphabet(self):
+        self.alphabet = ['a', 'b', 'c']
+
+    def setup_bound_calculator(self, start_nodes, bound_side_effect=bound_side_effect_cb_best):
+        bound_calculator_mock = MagicMock()
+        bound_calculator_mock.get_start_nodes.return_value = start_nodes
+        bound_calculator_mock.create_node.side_effect = bound_side_effect
+        self.bound_calculator = NodeCreatorMock(bound_calculator_mock)
+
+    def setup_bound_calculator_node_side_effect(self, start_nodes, bound_side_effect=bound_side_effect_cb_best):
+        self.bound_calculator_mock = MagicMock()
+        self.bound_calculator_mock.get_start_nodes.side_effect = start_nodes
+        self.bound_calculator_mock.create_node.side_effect = bound_side_effect
+        self.bound_calculator = NodeCreatorMock(self.bound_calculator_mock)
+
+    def setup_nodes(self):
+        self.b_node_bound = 2
+        self.cb_node_bound = 1.5
+        self.bc_node_bound = 1.5
+        self.cc_node_bound = 1.6
+        self.acb_node_bound = 1.4
+        self.bbb_node_bound = 1.3
+        self.abb_node_bound = 1.3
+        self.a_b_c_nodes = [MaxNode('a', 0., 1., 0.), MaxNode('b', 2., 1., 2.), MaxNode('c', 1., 1., 1.)]
+        self.a_b_c_nodes_large_c = [MaxNode('a', 0., 0, 0.), MaxNode('b', 2., 0, 0), MaxNode('c', 1.6, 0, 0)]
+
+    def test_one_gram_length_one_to_two_branch_and_bound_returns_max_start_node(self):
+        min_length = 1
+        max_length = 2
+        self.setup_bound_calculator_node_side_effect(start_node_side_effect_b_length_one_best)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, min_length, max_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'b')
+        self.assertEqual(y_bound, self.b_node_bound)
+
+    def test_length_one_to_two_length_two_better_node_branch_and_bound_returns_first_solution(self):
+        min_length = 1
+        max_length = 2
+        self.setup_bound_calculator_node_side_effect(start_node_side_effect_b_length_two_best,
+                                                     bound_side_effect_cb_best)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, min_length, max_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'b')
+        self.assertEqual(y_bound, self.b_node_bound)
+
+    def test_length_one_to_two_length_two_better_solution_branch_and_bound_returns_length_two_solution(self):
+        min_length = 1
+        max_length = 2
+        self.setup_bound_calculator_node_side_effect(start_node_side_effect_b_length_two_solution_best,
+                                                     bound_side_effect_cb_best)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, min_length, max_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'cb')
+        self.assertEqual(y_bound, self.cb_node_bound)
+
+    def test_length_one_to_three_length_two_better_solution_branch_and_bound_returns_length_two_solution(self):
+        min_length = 1
+        max_length = 3
+        self.setup_bound_calculator_node_side_effect(start_node_side_effect_start_at_b_length_two,
+                                                     bound_side_effect_bc_best_multiple_lengths)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, min_length, max_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'bc')
+        self.assertEqual(y_bound, self.cb_node_bound)
+
+    def test_one_gram_length_one_branch_and_bound_returns_max_start_node(self):
+        y_length = 1
+        self.setup_bound_calculator(self.a_b_c_nodes)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, y_length, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'b')
+        self.assertEqual(y_bound, self.b_node_bound)
+
+    def test_one_gram_length_two_branch_and_bound_returns_best_node(self):
+        y_length = 2
+        self.setup_bound_calculator(self.a_b_c_nodes)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, y_length, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'cb')
+        self.assertEqual(y_bound, self.cb_node_bound)
+
+    def test_one_gram_length_two_node_in_heap_better_than_first_solution_branch_and_bound_returns_best_node(self):
+        y_length = 2
+        self.setup_bound_calculator(self.a_b_c_nodes_large_c)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, y_length, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'cb')
+        self.assertEqual(y_bound, self.cb_node_bound)
+
+    def test_one_gram_length_two_other_solution_better_than_first_solution_branch_and_bound_returns_best_node(self):
+        y_length = 2
+        self.setup_bound_calculator(self.a_b_c_nodes_large_c, bound_side_effect_cc_best)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, y_length, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'cc')
+        self.assertEqual(y_bound, self.cc_node_bound)
+
+    def test_one_gram_length_three_branch_and_bound_returns_best_node(self):
+        y_length = 3
+        self.setup_bound_calculator(self.a_b_c_nodes, bound_side_effect_acb_best)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, y_length, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'acb')
+        self.assertEqual(y_bound, self.acb_node_bound)
+
+    def test_one_gram_length_three_other_solution_better_than_first_three_branch_and_bound_returns_best_node(self):
+        y_length = 3
+        self.setup_bound_calculator(self.a_b_c_nodes, bound_side_effect_bbb_best)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, y_length, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'bbb')
+        self.assertEqual(y_bound, self.bbb_node_bound)
+
+    def test_one_gram_length_three_other_nodes_better_than_solution_branch_and_bound_returns_best_node(self):
+        y_length = 3
+        self.setup_bound_calculator(self.a_b_c_nodes_large_c, bound_side_effect_abb_best_with_two_heap_pop)
+
+        y, y_bound = branch_and_bound_no_length(self.bound_calculator, y_length, y_length, self.alphabet, self.time)
+
+        self.assertEqual(y, 'abb')
+        self.assertEqual(y_bound, self.abb_node_bound)
+
+
+if __name__ == '__main__':
+    unittest2.main()
