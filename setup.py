00693c7ab53773c73176a612f5f8c77b2fd1db1c
Amelie <amelie.rolland.1@gmail.com>
2015-05-28 13:51:43 -0400

    add file to compile cython

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+import numpy
+from Cython.Distutils import build_ext
+
+
+ext_modules = [
+    Extension("preimage.kernels._generic_string", ["preimage/kernels/_generic_string.pyx"],
+              include_dirs=[numpy.get_include()]),
+    Extension("preimage.inference._branch_and_bound", ["preimage/inference/_branch_and_bound.pyx"],
+              include_dirs=[numpy.get_include()]),
+    Extension("preimage.inference._bound_calculator", ["preimage/inference/_bound_calculator.pyx"],
+              include_dirs=[numpy.get_include()]),
+    Extension("preimage.inference._node", ["preimage/inference/_node.pyx"],
+              include_dirs=[numpy.get_include()]),
+    Extension("preimage.inference._node_creator", ["preimage/inference/_node_creator.pyx"],
+              include_dirs=[numpy.get_include()])
+]
+
+setup(
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules
+)
