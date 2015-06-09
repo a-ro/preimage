d6fbcaced5a88223ec36da934d5b5c5746f4009d
Amelie <amelie.rolland.1@gmail.com>
2015-06-09 11:38:17 -0400

    add min node for branch and bound with multiple solutions

--- preimage/inference/node.pxd
+++ preimage/inference/node.pxd
@@ -4,10 +4,16 @@ cimport numpy
 ctypedef numpy.float64_t FLOAT64_t
 
 
-cdef class MaxNode:
+cdef class Node:
     cdef public str y
     cdef public FLOAT64_t real_min_bound, real_max_bound, bound
-
-    cdef MaxNode copy(self)
-
     cdef FLOAT64_t get_bound(self)
+
+
+cdef class MaxNode(Node):
+    cdef FLOAT64_t get_bound(self)
+    cdef MinNode convert_to_min_node(self)
+
+
+cdef class MinNode(Node):
+    cdef MaxNode convert_to_max_node(self)
--- preimage/inference/node.pyx
+++ preimage/inference/node.pyx
@@ -1,33 +1,57 @@
 import numpy
 
 
-cdef class MaxNode:
-    # negative bound because we want to maximize
-    def __cinit__(self, y, bound, real_min_bound, real_max_bound):
+cdef class Node:
+    def __init__(self, y, bound, real_min_bound, real_max_bound):
         self.y = y
-        self.bound = -bound
+        self.bound = bound
         self.real_min_bound = real_min_bound
         self.real_max_bound = real_max_bound
 
-    # use random when equal bounds (n-gram kernel might have a non-unique pre-image)
-    # only < operators is implemented for heap sort and == operator for unit tests
+    def __str__(self):
+        node_string = "y: {}, bound: {}, real_min_bound: {}, real_max_bound: {}"
+        return node_string.format(self.y, self.get_bound(), self.real_min_bound, self.real_max_bound)
+
+    cdef FLOAT64_t get_bound(self):
+        return self.bound
+
+
+cdef class MaxNode(Node):
+    def __init__(self, y, bound, real_min_bound, real_max_bound):
+        Node.__init__(self, y, -bound, real_min_bound, real_max_bound)
+
     def __richcmp__(self, MaxNode other_node, int op):
         if op == 0:
             if self.bound == other_node.bound:
                 return numpy.random.randint(2)
             else:
                 return self.bound < other_node.bound
-        elif op == 2:
+        if op == 2:
             return self.y == other_node.y and self.bound == other_node.bound \
                    and self.real_min_bound == other_node.real_min_bound \
                    and self.real_max_bound == other_node.real_max_bound
 
-    def __str__(self):
-        node_string = "y: {}, bound: {}, real_min_bound: {}, real_max_bound: {}"
-        return node_string.format(self.y, self.get_bound(), self.real_min_bound, self.real_max_bound)
-
-    cdef MaxNode copy(self):
-        return MaxNode(self.y, self.get_bound(), self.real_min_bound, self.real_max_bound)
-
     cdef FLOAT64_t get_bound(self):
         return -self.bound
+
+    cdef MinNode convert_to_min_node(self):
+        return MinNode(self.y, self.get_bound(), self.real_min_bound, self.real_max_bound)
+
+
+cdef class MinNode(Node):
+    def __init__(self, y, bound, real_min_bound, real_max_bound):
+        Node.__init__(self, y, bound, real_min_bound, real_max_bound)
+
+    def __richcmp__(self, MinNode other_node, int op):
+        if op == 0:
+            if self.bound == other_node.bound:
+                return numpy.random.randint(2)
+            else:
+                return self.bound < other_node.bound
+        if op == 2:
+            return self.y == other_node.y and self.bound == other_node.bound \
+                   and self.real_min_bound == other_node.real_min_bound \
+                   and self.real_max_bound == other_node.real_max_bound
+
+    cdef MaxNode convert_to_max_node(self):
+        return MaxNode(self.y, self.get_bound(), self.real_min_bound, self.real_max_bound)
