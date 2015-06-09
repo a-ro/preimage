412580453643705784b907dbc67831916a202b6a
Amelie <amelie.rolland.1@gmail.com>
2015-06-09 10:58:29 -0400

    minor code refactor

--- preimage/examples/generic_string_ocr.py
+++ preimage/examples/generic_string_ocr.py
@@ -9,7 +9,7 @@ from preimage.metrics.structured_output import zero_one_loss, hamming_loss, leve
 
 
 if __name__ == '__main__':
-    # You should find best parameters with cross-validation
+    # You should find the best parameters with cross-validation
     poly_kernel = PolynomialKernel(degree=2)
     alpha = 1e-5
     n_predictions = 50
@@ -19,20 +19,25 @@ if __name__ == '__main__':
 
     print('Generic String model on OCR Letter Dataset')
     train_dataset, test_dataset = load_ocr_letters(fold_id=0)
+    test_dataset.X = test_dataset.X[0:n_predictions]
+    test_dataset.Y = test_dataset.Y[0:n_predictions]
+
     inference_model = GenericStringModel(Alphabet.latin, n, is_using_length, sigma_position=sigma_position)
     learner = StructuredKernelRidgeRegression(alpha, poly_kernel, inference_model)
 
-    print('\ntraining ...')
+    print('training ...')
     learner.fit(train_dataset.X, train_dataset.Y, train_dataset.y_lengths)
 
     print('predict ...')
-    Y_predictions = learner.predict(test_dataset.X[0:n_predictions], test_dataset.y_lengths[0:n_predictions])
+    Y_predictions = learner.predict(test_dataset.X, test_dataset.y_lengths)
 
-    print('\nY predictions: ', Y_predictions)
-    print('Y real: ', test_dataset.Y[0:n_predictions])
+    print('\n')
+    print('Y predictions: ', Y_predictions)
+    print('Y real: ', test_dataset.Y)
 
-    print('\nResults:')
-    print('zero_one_loss', zero_one_loss(test_dataset.Y[0:n_predictions], Y_predictions[0:n_predictions]))
-    print('levenshtein_loss', levenshtein_loss(test_dataset.Y[0:n_predictions], Y_predictions[0:n_predictions]))
+    print('\n')
+    print('Results:')
+    print('zero_one_loss', zero_one_loss(test_dataset.Y, Y_predictions))
+    print('levenshtein_loss', levenshtein_loss(test_dataset.Y, Y_predictions))
     if is_using_length:
-        print('hamming_loss', hamming_loss(test_dataset.Y[0:n_predictions], Y_predictions[0:n_predictions]))
+        print('hamming_loss', hamming_loss(test_dataset.Y, Y_predictions))
